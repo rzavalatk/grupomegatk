11b975a6b48dd4c55605ef177e3846bd8f45fcf4
megaaleja@gmail.com <megaaleja@gmail.com>
2018-05-22 21:23:06 -0600

    Actuaización

--- models/account_invoice.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from odoo import models, fields, api, _
-from odoo.exceptions import Warning
-
-class AccountInvoice(models.Model):
-    _inherit = "account.invoice"
-
-    numero_factura = fields.Char('Número de factura', help='Número de factura')
-    cai_suplidor = fields.Char("CAI de Proveedor")
