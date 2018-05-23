3e9d8de41170d5cd5567efcfe8f48d48ab3ead7b
megaaleja@gmail.com <megaaleja@gmail.com>
2018-05-22 21:41:21 -0600

    Actualización de bancos

--- banks/models/account_invoice.py
+++ banks/models/account_invoice.py
@@ -1,9 +1,8 @@
 # -*- coding: utf-8 -*-
-from odoo import models, fields, api, _
-from odoo.exceptions import Warning
+from odoo import api, fields, models
 
 class AccountInvoice(models.Model):
     _inherit = "account.invoice"
 
     numero_factura = fields.Char('Número de factura', help='Número de factura')
-    #cai_suplidor = fields.Char("CAI de Proveedor")
+    cai_suplidor = fields.Char("CAI de Proveedor")
