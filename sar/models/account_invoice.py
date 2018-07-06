4a2b2e7cfb8fbe39becaafe6f3cd833f44427a06
Romel Zavala <rzavala@megatk.com>
2018-07-06 13:02:36 -0600

    Update account_invoice.py

--- sar/models/account_invoice.py
+++ sar/models/account_invoice.py
@@ -2,6 +2,7 @@
 
 from odoo import models, fields, api, _
 from odoo.exceptions import Warning
+from datetime import datetime, date
 #from itertools import ifilter
 # mapping invoice type to journal type
 TYPE2JOURNAL = {
