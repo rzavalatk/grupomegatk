3ef20dd1149cd0887c0f31baf3e86030575273bb
Romel Zavala <rzavala@megatk.com>
2018-09-18 13:35:15 -0600

    CRM taller

--- fields_megatk/models/crm_lead.py
+++ fields_megatk/models/crm_lead.py
@@ -15,7 +15,7 @@ class CrmLead(models.Model):
     tipo_id = fields.Many2one('crm.lead.tipo', string='Título de oportunidad',)
     tipo_soporte = fields.Selection([('llamada','Llamada'),('visita','Visita'),('taller','Taller')], string='Tipo de Soporte', default='visita')
     estado_taller = fields.Selection([('sinrevision','Sin Revisión'),('enreparacion','En Reparación'),('diagnostico','Diagnóstico'),('pendaprov','Pend. Aprobación'),('rma','RMA'),('reparado','Reparado')], string='Estado del Equipo', default='sinrevision')
-    tipo_visita = fields.Selection([('cortesia','Cortesía'),('contado','Contado'),('garantia','Garantía'),('capacitacion','Capacitación')], string='Tipo de Visita', default='visita')
+    tipo_visita = fields.Selection([('cortesia','Cortesía'),('contado','Contado'),('garantia','Garantía'),('capacitacion','Capacitación')], string='Tipo de Visita')
     reporto = fields.Char(string='Persona que reporto',)
     repor_tel = fields.Char(string='Telefono',)
     repor_email = fields.Char(string='Correo electrónico',)
