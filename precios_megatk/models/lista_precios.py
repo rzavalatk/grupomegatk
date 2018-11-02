f1957ca18adc0c2ad2487a78ae450f00783630ea
Romel Zavala <rzavala@megatk.com>
2018-11-01 22:21:05 -0600

    Update lista_precios.py

--- precios_megatk/models/lista_precios.py
+++ precios_megatk/models/lista_precios.py
@@ -62,7 +62,6 @@ class ListaPreciosLine(models.Model):
     _name = "lista.precios.megatk.line"
 
     obj_padre = fields.Many2one("lista.precios.megatk", "Precio", ondelete='cascade')
-    company_id = fields.Many2one('obj_padre.company_id', "Empresa", )
     product_id = fields.Many2one("product.product", "Producto", required=True, )
     precio_publico = fields.Float("Precio Base", readonly=True)
     precio_descuento = fields.Float("Precio de lista", readonly=True)
