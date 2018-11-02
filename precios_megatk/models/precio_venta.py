64aab09315549a2f61ef50cf34c9737b9b5c862e
Romel Zavala <rzavala@megatk.com>
2018-11-02 10:03:24 -0600

    Update precio_venta.py

--- precios_megatk/models/precio_venta.py
+++ precios_megatk/models/precio_venta.py
@@ -8,8 +8,8 @@ class Precioproducto(models.Model):
     _name = "lista.precios.producto"
     _order = 'name asc'
 
-    name = fields.Many2one("lista.precios.tipo.descuento", "Margen", required=True, ondelete='cascade')
+    name = fields.Many2one("lista.precios.tipo.descuento", "Margen", required=True,)
     product_id = fields.Many2one("product.product", "Producto", required=True)
-    lista_id = fields.Many2one("lista.precios.megatk", "Lista de precios", ondelete='cascade')
+    lista_id = fields.Many2one("lista.precios.megatk", "Lista de precios",)
     descuento = fields.Float("Margen")
     precio = fields.Float("Precio")
