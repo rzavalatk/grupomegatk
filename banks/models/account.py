6b33fa0f4a98056e1e5d38d5eb87b0e162ca168a
Romel Zavala <rzavala@megatk.com>
2019-07-17 17:04:14 -0600

    Movimientos automaticos

--- banks/models/account.py
+++ banks/models/account.py
@@ -9,16 +9,18 @@ class Account(models.Model):
 
 
 class AccountInvoice(models.Model):
-    _inherit = "account.move.line"
+	_inherit = "account.move.line"
 
-    @api.model
-    def create(self,values):
-    	account = self.env['account.account'].browse(values['account_id'])
-    	if (account.user_type_id.id == 16 or account.user_type_id.id == 17) and self.env.user.id!=1:
-    		if not 'analytic_account_id' in values:
-    			values['analytic_account_id']=account.analytic_id.id
-    		
-    		if not values['analytic_account_id']:
-	    		raise Warning(_('Cuenta analitica requerida en la linea con cuenta: '+ account.code+' '+account.name+' descripción: '+values['name']))
-    	return super(AccountInvoice, self).create(values)
-
+	@api.model
+	def create(self,values):
+		account = self.env['account.account'].browse(values['account_id'])
+		if (account.user_type_id.id == 16 or account.user_type_id.id == 17) and self.env.user.id!=1:
+			if not 'analytic_account_id' in values:
+				values['analytic_account_id']=account.analytic_id.id
+			if not values['analytic_account_id']:
+				if account.analytic_id:
+					values['analytic_account_id']=account.analytic_id.id
+				else:
+					raise Warning(_('Cuenta analitica requerida en la linea con cuenta: '+ account.code+' '+account.name+' descripción: '+values['name']))
+		
+		return super(AccountInvoice, self).create(values)
