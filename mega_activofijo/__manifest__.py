d210a07ce20204a11b57c3f5699f594763589dc6
Romel Zavala <rzavala@megatk.com>
2019-08-26 13:47:51 -0600

    activo fijo: información de Activo

---
+++ mega_activofijo/__manifest__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "mega_activofijo",
+
+    'summary': """
+        personalizacion del modulo de activo fijo""",
+
+    'description': """
+        personalizacion del modulo de activo fijo
+    """,
+
+    'author': "Romel Zavala",
+    'website': "www.megatk.net",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base','account_asset','hr','product'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/acyivo_fijo_view.xml',
+    ],
+}
---
+++ mega_activofijo/models/models.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+class ActivoFijo(models.Model):
+    _inherit = 'account.asset.asset'
+
+    employee_id = fields.Many2one('hr.employee', string='Responsable',)
+    departamento = fields.Char(related='employee_id.department_id.name', string="Departamento")
+
+    marca = fields.Char(string='Marca',)
+    modelo = fields.Char(string='Modelo',)
+    serie = fields.Char(string='Serie',)
+    descripcion = fields.Text(string='Descripción',)
+
+
+    
