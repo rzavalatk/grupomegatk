d13242a76ba87cd158b1ebdfa47792d4440f5f22
Romel Zavala <rzavala@megatk.com>
2019-10-24 17:16:30 -0600

    actualización

--- importacion/__manifest__.py
+++ importacion/__manifest__.py
@@ -19,7 +19,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','purchase','stock'],
+    'depends': ['base','purchase','stock','purchase_stock'],
 
     # always loaded
     'data': [
