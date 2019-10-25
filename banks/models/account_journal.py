0fe6174a2feaf1dc66494b3e48219c6c5896dec4
Romel Zavala <rzavala@megatk.com>
2019-10-24 20:55:21 -0600

    Actulizacion

--- banks/models/account_journal.py
+++ banks/models/account_journal.py
@@ -4,7 +4,7 @@ from odoo import models, fields, api, _
 
 
 class AccountJournal(models.Model):
-	_inherit = "account.journal"
+    _inherit = "account.journal"
 
-	multi_seq_banks = fields.Boolean('Multi secuencias bancarias', default=False)
-	secuencia_ids = fields.One2many("ir.sequence", "journal_id", "Secuencias")
+    multi_seq_banks = fields.Boolean('Multi secuencias bancarias', default=False)
+    secuencia_ids = fields.One2many("ir.sequence", "journal_id", "Secuencias")
