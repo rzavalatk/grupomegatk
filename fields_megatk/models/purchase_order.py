6b87e6b98ff2aa746d47dc223e86f8b9f9211b22
Romel Zavala <rzavala@megatk.com>
2020-07-10 14:15:59 -0600

    campo Etapa de orden de compra

--- fields_megatk/models/purchase_order.py
+++ fields_megatk/models/purchase_order.py
@@ -6,8 +6,8 @@ class Purchase(models.Model):
     _inherit = "purchase.order"
 
     x_enviar = fields.Text("Enviar a")
-    
-
+    etapa_orden = fields.Selection([('orden_p_s','Orden Pendiente de Solicitud'),('orden_p_t','Orden Pendiente de Transferencia'),('orden_p_d','Orden Pendiente de Despacho'),('orden_t','Orden en Tránsito'),('orden_a','Producto en Aduana'),('orden_i','Ingresó a Bodega')],string='Etapa de orden',default='orden_p_s')
+        
 class PurchaseLine(models.Model):
     _inherit = "purchase.order.line"
 
