742883bbee7f8930a3332c6a91552c3fb7faf70d
Romel Zavala <rzavala@megatk.com>
2021-08-20 09:42:38 -0600

    Update purchase_order.py

--- fields_megatk/models/purchase_order.py
+++ fields_megatk/models/purchase_order.py
@@ -6,7 +6,7 @@ class Purchase(models.Model):
     _inherit = "purchase.order"
 
     x_enviar = fields.Text("Enviar a")
-    etapa_orden = fields.Selection([('orden_p_s','Orden Pendiente de Solicitud'),('orden_p_t','Orden Pendiente de Transferencia'),('orden_p_d','Orden Pendiente de Despacho'),('orden_t','Orden en Tránsito'),('orden_a','Producto en Aduana'),('orden_i','Ingresó a Bodega')],string='Etapa de orden',default='orden_p_s')
+    etapa_orden = fields.Selection([('orden_p_s','Orden Pendiente de Solicitud'),('orden_p_t','Orden Pendiente de Transferencia'),('orden_p_d','Orden Pendiente de Despacho'),('orden_t','Orden en Tránsito'),('orden_a','Producto en Aduana'),('orden_i','Ingresó a Bodega'),('orden_l','Listo')],string='Etapa de orden',default='orden_p_s')
         
 class PurchaseLine(models.Model):
     _inherit = "purchase.order.line"
