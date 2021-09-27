b5883e05c3d7bd49b9e393529772fd5b6c39c216
Alejandro Zelaya <jandro.zg123@gmail.com>
2021-09-27 14:21:00 -0600

    nuevos campos para compras

---
+++ fields_compras/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Campos compras",
+
+    'summary': """
+        
+        """,
+
+    'description': """
+        
+    """,
+
+    'author': "Alejandro Zelaya",
+    'website': "azelaya@megatk.com",
+    'category': 'Usuarios',
+    'version': '0.1',
+    'depends': ['base','purchase',],
+    'data': [
+        'views/view.xml',
+    ],
+}
---
+++ fields_compras/models/models.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, api, fields
+
+
+class ModelCompras(models.Model):
+    _inherit = 'purchase.order'
+
+    @api.one
+    def _count_compute(self):
+        self.write({
+            "count": len(self.order_line) + 0.0
+        })
+        self.count_compute = len(self.order_line) + 0.0
+
+
+    cubing = fields.Float("Cubicaje total")
+    weight = fields.Float("Peso total")
+    count = fields.Float()
+    count_compute = fields.Float("Total",compute=_count_compute)
