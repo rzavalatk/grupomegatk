18185e6a68012a51d3e517caf894ee2bbc112df8
Alejandro Zelaya <jandro.zg123@gmail.com>
2021-09-30 11:37:01 -0600

    actualizacion de modulos website y hide_price_public_user

--- hide_price_pubilc_user/__manifest__.py
+++ hide_price_pubilc_user/__manifest__.py
@@ -10,25 +10,16 @@
         En este modulo se le oculta el precio al usuario publico
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Alejandro Zelaya",
+    'website': "azelaya@megatk.com",
 
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
     'category': 'Users',
     'version': '0.1',
 
-    # any module necessary for this one to work correctly
-    'depends': ['base', 'website'],
+    'depends': ['base', 'website', 'ks_theme_kinetik'],
 
-    # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'views/price.xml',
         'views/views.xml',
-    ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
     ],
 }
