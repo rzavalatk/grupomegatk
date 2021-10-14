835f5ea76c8cccfbdd809597deac09a8fad6e461
Alejandro Zelaya <jandro.zg123@gmail.com>
2021-10-14 16:02:54 -0600

    se añadio el campo de ciudad de origen de una compra

--- fields_compras/models/models.py
+++ fields_compras/models/models.py
@@ -6,16 +6,8 @@ from odoo import models, api, fields
 class ModelCompras(models.Model):
     _inherit = 'purchase.order'
 
-    @api.one
-    def _count_compute(self):
-        self.write({
-            "count": len(self.order_line) + 0.0
-        })
-        self.count_compute = len(self.order_line) + 0.0
-
 
     cubing = fields.Float("Cubicaje total")
     weight = fields.Float("Peso total")
     code_reference = fields.Char("Código de Referencia")
-    count = fields.Float()
-    count_compute = fields.Float("Total",compute=_count_compute)
+    origin_city = fields.Char("Ciudad de origen")
