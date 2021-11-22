42c58d93e260100e9e62628e73f5d926b96da280
Alejandro Zelaya <jandro.zg123@gmail.com>
2021-11-22 15:21:08 -0600

    reporte customizado

--- website_custom/models/models.py
+++ website_custom/models/models.py
@@ -16,11 +16,13 @@ class BreadcumCustom(models.Model):
     ]
     
     def generate_report(self):
-        sql = """
+        id = self.env.user.company_id.id
+        sql = f"""
         SELECT  date_invoice as Fecha,
         (SELECT name FROM res_partner as r WHERE r.id = a.partner_id) as Cliente, 
         count(partner_id) as Numero_de_facturas FROM  account_invoice as a 
         WHERE state='paid' AND date_invoice BETWEEN '2021-01-01' AND '2021-12-31' 
+        AND company_id = {id}
         GROUP BY partner_id, date_invoice  
         ORDER BY  date_invoice asc
         """
