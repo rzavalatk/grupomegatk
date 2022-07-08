4e20d15005ab750ad0db4277c9efc566b4a04664
Alejandro Zelaya <jandro.zg123@gmail.com>
2022-07-08 11:35:16 -0600

    agregar el sin costo en CRM

--- fields_megatk/models/crm_lead.py
+++ fields_megatk/models/crm_lead.py
@@ -14,7 +14,7 @@ class CrmLead(models.Model):
     fallas = fields.Text("Fallas")
     informe_tecnico = fields.Text("Informe técnico")
     tipo_id = fields.Many2one('crm.lead.tipo', string='Título de oportunidad',)
-    tipo_soporte = fields.Selection([('llamada','Llamada'),('interno','Interno'),('visita','Visita'),('taller','Taller')], string='Tipo de Soporte', default='llamada')
+    tipo_soporte = fields.Selection([('llamada','Llamada'),('interno','Interno'),('visita','Visita'),('taller','Taller'),('sin_costo','Sin Costo')], string='Tipo de Soporte', default='llamada')
     estado_taller = fields.Selection([('sinrevision','Sin Revisión'),('enreparacion','En Reparación'),('diagnostico','Diagnóstico'),('pendaprov','Pend. Aprobación'),('rma','RMA'),('reparado','Reparado'),('equipo_x_entregar','Equipo por entregar'),('entregado','Entregado')], string='Estado del Equipo', default='sinrevision')
     tipo_visita = fields.Selection([('cortesia','Cortesía'),('contado','Contado'),('garantia','Garantía'),('capacitacion','Capacitación'),('instalacion','Instalación')], string='Tipo de Visita')
     tipo_venta = fields.Selection([('llamada','Llamada'),('visitac','Visita Calle'),('visitat','Visita Tienda')], string='Venta', default='llamada')
