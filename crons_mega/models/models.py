c543f323fbae579261b3f0c6e305f7b7ff5a125a
Alejandro Zelaya <jandro.zg123@gmail.com>
2022-10-20 11:35:05 -0600

    modificaciones a cron de cierre diario

--- crons_mega/models/models.py
+++ crons_mega/models/models.py
@@ -8,12 +8,13 @@ class Pagos(models.Model):
     _inherit = "account.payment"
     
     
-    def send_email(self,values):
+    def send_email(self,values,to):
         template = self.env.ref(
             'crons_mega.email_template_cierre_diario')
         email_values = {
-            'email_to': 'azelaya@megatk.com',
-            'email_from': ['mmoran@megatk.com']
+            'email_from': 'azelaya@megatk.com',
+            'email_to': to,
+            'email_cc': 'azelaya@megatk.com'
         }
         template.with_context(values).send_mail(self.id, email_values=email_values, force_send=True)
         return True
@@ -24,16 +25,8 @@ class Pagos(models.Model):
             return True
         except:
             return False
-
-    def get_payments_today(self):
-        today = date.today()
-        value = {}
-        value['date'] = today.strftime('%Y-%m-%d')
-        payments = self.env['account.payment'].search(
-            [('payment_date', '=', today.strftime('%Y-%m-%d'))])
-        obj = []
-        for item in payments:
-            obj.append((item.region, item.journal_id.name, item.amount))
+        
+    def _reoder_data(self,obj):
         mydict = {}
         for item in obj:
             if self._exist_index(mydict, item[0]):
@@ -45,5 +38,57 @@ class Pagos(models.Model):
                 mydict[item[0]] = {
                     item[1]: item[2]
                 }
-        value['accounts'] = mydict
-        self.send_email(value)
+        return mydict
+    
+    def _check_only_currency(self,amount,current,current_final):
+        if current.name == current_final:
+            return amount
+        else:
+            return amount/current.rate
+
+    def get_payments_today(self):
+        today = date.today()
+        value = {}
+        value['date'] = today.strftime('%Y-%m-%d')
+        mega = self.env['res.company'].search([('name','=','MEGATK')])
+        meditek = self.env['res.company'].search([('name','=','MEDITEK')])
+        mediteksa = self.env['res.company'].search([('name','=','MEDITEKSA NIC')])
+        for item in mega:
+            mega_id = item.id
+        for item in meditek:
+            medi_id = item.id
+        for item in mediteksa:
+            medisa_id = item.id     
+        payments_mega = self.env['account.payment'].search(
+            ['&',('payment_date', '=', today.strftime('%Y-%m-%d')),
+             ('company_id', '=', mega_id)
+             ])
+        payments_medi = self.env['account.payment'].search(
+            ['&',('payment_date', '=', today.strftime('%Y-%m-%d')),
+             ('company_id', '=', medi_id)
+             ])
+        payments_medisa = self.env['account.payment'].search(
+            ['&',('payment_date', '=', today.strftime('%Y-%m-%d')),
+             ('company_id', '=', medisa_id)
+             ])
+        obj_mega = []
+        obj_medi = []
+        obj_medisa = []
+        for item in payments_mega:
+            amount = self._check_only_currency(item.amount,item.currency_id,'HNL')
+            obj_mega.append((item.region, item.journal_id.name, amount))
+        for item in payments_medi:
+            amount = self._check_only_currency(item.amount,item.currency_id,'HNL')
+            obj_medi.append((item.region, item.journal_id.name, amount))
+        for item in payments_medisa:
+            amount = self._check_only_currency(item.amount,item.currency_id,'USD')
+            obj_medisa.append((item.region, item.journal_id.name, amount))
+        dict_mega = self._reoder_data(obj_mega)
+        dict_medi = self._reoder_data(obj_medi)
+        dict_medisa = self._reoder_data(obj_medisa)
+        value['accounts'] = {**dict_mega,**dict_medisa}
+        value['company'] = "MegaTK y Mediteksa NIC"
+        self.send_email(value,"lmoran@megatk.com")
+        value['company'] = "Mediteksa"
+        value['accounts'] = dict_medi
+        self.send_email(value,"jmoran@meditekhn.com")
