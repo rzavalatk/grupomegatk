46ad5c1d6e3ec462c5118c6a3628f62301ab2634
Alejandro Zelaya <jandro.zg123@gmail.com>
2022-10-24 08:12:35 -0600

    descartar que mande correos los domingos

--- crons_mega/models/models.py
+++ crons_mega/models/models.py
@@ -52,50 +52,51 @@ class Pagos(models.Model):
     def get_payments_today(self):
         tz = pytz.timezone("America/Guatemala")
         today_datetime = datetime.now(tz)
-        today = today_datetime.strftime('%Y-%m-%d')
-        value = {}
-        value['date'] = today_datetime.strftime('%d/%m/%Y')
-        mega = self.env['res.company'].search([('name','=','MEGATK')])
-        meditek = self.env['res.company'].search([('name','=','MEDITEK')])
-        mediteksa = self.env['res.company'].search([('name','=','MEDITEKSA NIC')])
-        for item in mega:
-            mega_id = item.id
-        for item in meditek:
-            medi_id = item.id
-        for item in mediteksa:
-            medisa_id = item.id     
-        payments_mega = self.env['account.payment'].search(
-            ['&',('payment_date', '=', today),
-             ('company_id', '=', mega_id)
-             ])
-        payments_medi = self.env['account.payment'].search(
-            ['&',('payment_date', '=', today),
-             ('company_id', '=', medi_id)
-             ])
-        payments_medisa = self.env['account.payment'].search(
-            ['&',('payment_date', '=', today),
-             ('company_id', '=', medisa_id)
-             ])
-        obj_mega = []
-        obj_medi = []
-        obj_medisa = []
-        for item in payments_mega:
-            amount = self._check_only_currency(item.amount,item.currency_id,'HNL')
-            obj_mega.append((item.region, item.journal_id.name, amount))
-        for item in payments_medi:
-            amount = self._check_only_currency(item.amount,item.currency_id,'HNL')
-            obj_medi.append((item.region, item.journal_id.name, amount))
-        for item in payments_medisa:
-            amount = self._check_only_currency(item.amount,item.currency_id,'USD')
-            obj_medisa.append((item.region, item.journal_id.name, amount))
-        dict_mega = self._reoder_data(obj_mega)
-        dict_medi = self._reoder_data(obj_medi)
-        dict_medisa = self._reoder_data(obj_medisa)
-        value['accounts'] = {**dict_mega,**dict_medisa}
-        value['company'] = "MegaTK y Mediteksa NIC"
-        # self.send_email(value,"azelaya@megatk.com")
-        self.send_email(value,"lmoran@megatk.com")
-        value['company'] = "Mediteksa"
-        value['accounts'] = dict_medi
-        # self.send_email(value,"azelaya@megatk.com")
-        self.send_email(value,"jmoran@meditekhn.com")
+        if today_datetime.weekday() != 6:        
+            today = today_datetime.strftime('%Y-%m-%d')
+            value = {}
+            value['date'] = today_datetime.strftime('%d/%m/%Y')
+            mega = self.env['res.company'].search([('name','=','MEGATK')])
+            meditek = self.env['res.company'].search([('name','=','MEDITEK')])
+            mediteksa = self.env['res.company'].search([('name','=','MEDITEKSA NIC')])
+            for item in mega:
+                mega_id = item.id
+            for item in meditek:
+                medi_id = item.id
+            for item in mediteksa:
+                medisa_id = item.id     
+            payments_mega = self.env['account.payment'].search(
+                ['&',('payment_date', '=', today),
+                ('company_id', '=', mega_id)
+                ])
+            payments_medi = self.env['account.payment'].search(
+                ['&',('payment_date', '=', today),
+                ('company_id', '=', medi_id)
+                ])
+            payments_medisa = self.env['account.payment'].search(
+                ['&',('payment_date', '=', today),
+                ('company_id', '=', medisa_id)
+                ])
+            obj_mega = []
+            obj_medi = []
+            obj_medisa = []
+            for item in payments_mega:
+                amount = self._check_only_currency(item.amount,item.currency_id,'HNL')
+                obj_mega.append((item.region, item.journal_id.name, amount))
+            for item in payments_medi:
+                amount = self._check_only_currency(item.amount,item.currency_id,'HNL')
+                obj_medi.append((item.region, item.journal_id.name, amount))
+            for item in payments_medisa:
+                amount = self._check_only_currency(item.amount,item.currency_id,'USD')
+                obj_medisa.append((item.region, item.journal_id.name, amount))
+            dict_mega = self._reoder_data(obj_mega)
+            dict_medi = self._reoder_data(obj_medi)
+            dict_medisa = self._reoder_data(obj_medisa)
+            value['accounts'] = {**dict_mega,**dict_medisa}
+            value['company'] = "MegaTK y Mediteksa NIC"
+            # self.send_email(value,"azelaya@megatk.com")
+            self.send_email(value,"lmoran@megatk.com")
+            value['company'] = "Mediteksa"
+            value['accounts'] = dict_medi
+            # self.send_email(value,"azelaya@megatk.com")
+            self.send_email(value,"jmoran@meditekhn.com")
