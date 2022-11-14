9e520460fdf0fb7d10717d331b06debc8ff06f11
Alejandro Zelaya <jandro.zg123@gmail.com>
2022-11-14 16:06:45 -0600

    limites a vendedores

---
+++ grupos_accesos/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Grupos de accesos",
+
+    'summary': """
+        Modulo para manejar grupos de accesos
+        """,
+
+    'description': """
+        Modulo para manejar grupos de accesos
+    """,
+
+    'author': "Alejandro Zelaya",
+    'website': "azelaya@megatk.com",
+
+    'category': 'Usuarios',
+    'version': '0.1',
+
+    'depends': ['base','sale','res','account'],
+
+    'data': [
+        'security/groups.xml',
+        'security/edit_responsable.xml',
+    ],
+}
