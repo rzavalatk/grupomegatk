123f3bb322ee92d2a879f59955f6439ff108ab2e
Alejandro Zelaya <jandro.zg123@gmail.com>
2022-12-14 16:42:04 -0600

    modulo de pruebas

--- modulo_para_pruebas/models/models.py
+++ modulo_para_pruebas/models/models.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, api, fields
-
+from datetime import datetime
 
 class Logs(models.Model):
     _name="logs.model"
@@ -14,20 +14,36 @@ class Logs(models.Model):
 class Gastos(models.Model):
     _inherit="account.move"
     
-    @api.model
-    def create(self, vals):
-        res = super(Gastos,self).create(vals)
-        # self.env.context.get
+    @api.multi
+    def assert_balanced(self):
+        today = datetime.today()
         text = ""
-        text += "////////////Create: "+str(res)+"//////////////"
-        text += "\n"
-        text += "////////////Vals: "+str(vals)+"//////////////"
-        text += "\n"
         text += "////////////Context: "+str(self.env.context)+"//////////////"
+        text += "\n"
+        text += "////////////Context: "+str(self.ids)+"//////////////"
         
         self.env['logs.model'].create({
-            'name': self._name,
+            'name': self._name +" : "+ str(today),
             'register': text
         })
-        
+        res = super(Gastos,self).assert_balanced()
+    
         return res
+    
+    # @api.model
+    # def create(self, vals):
+    #     res = super(Gastos,self).create(vals)
+    #     # self.env.context.get
+    #     text = ""
+    #     text += "////////////Create: "+str(res)+"//////////////"
+    #     text += "\n"
+    #     text += "////////////Vals: "+str(vals)+"//////////////"
+    #     text += "\n"
+    #     text += "////////////Context: "+str(self.env.context)+"//////////////"
+        
+    #     self.env['logs.model'].create({
+    #         'name': self._name,
+    #         'register': text
+    #     })
+        
+    #     return res
