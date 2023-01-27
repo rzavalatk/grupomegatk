973043f99630d856df7c386ca85ecb9005952b0f
Alejandro Zelaya <jandro.zg123@gmail.com>
2023-01-27 10:52:21 -0600

    modulo nuevo para ver tarjetas en pasivos

---
+++ informe_tarjetas/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Agregar pagos de tarjetas",
+
+    'summary': """
+        Modulo para visualizar los pagos a tarjetas en bancos en informe de historicos de pagos
+        """,
+
+    'description': """
+        Modulo para visualizar los pagos a tarjetas en bancos en informe de historicos de pagos
+    """,
+
+    'author': "Alejandro Zelaya",
+    'website': "azelaya@megatk.com",
+    'category': 'Usuarios',
+    'version': '0.1',
+
+    'depends': ['base','account_reports'],
+
+    'data': [
+        'views/account_acoount.xml'
+    ],
+}
