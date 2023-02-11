a8c2d63742060e57c8aeaf5ad117b75efc228824
Alejandro Zelaya <jandro.zg123@gmail.com>
2023-02-11 10:13:16 -0600

    actualizacion en payment term

--- payment_term_custom/models/models.py
+++ payment_term_custom/models/models.py
@@ -7,3 +7,13 @@ class TermPament(models.Model):
     _inherit = "account.payment.term"
     
     public = fields.Boolean("Publico",default=False)
+    
+class Facturas(models.Model):
+    _inherit = "account.invoice"
+    
+    @api.one
+    def _payment_term_compute(self):
+        self.payment_term_compute = self.payment_term_id.sudo().name
+        
+    
+    payment_term_compute = fields.Char("Plazo de pago(computado)",compute=_payment_term_compute)
