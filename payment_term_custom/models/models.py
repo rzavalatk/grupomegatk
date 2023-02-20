03210225d483028a4a7d78e319d4bb70a1a3cd37
Alejandro Zelaya <jandro.zg123@gmail.com>
2023-02-20 09:38:58 -0600

    actualizacion de terminos de pago

--- payment_term_custom/models/models.py
+++ payment_term_custom/models/models.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, api,fields
+from odoo import models, api,fields,_
+from odoo.exceptions import Warning
 
 
 class TermPament(models.Model):
@@ -17,3 +18,56 @@ class Facturas(models.Model):
         
     
     payment_term_compute = fields.Char("Plazo de pago(computado)",compute=_payment_term_compute)
+    
+    
+    @api.model
+    def create(self,vals):
+        try:
+            payment_term = self.env['account.payment.term'].browse(vals['payment_term_id'])
+            payment_term.name
+            res = super(Facturas,self).create(vals)
+            return res
+        except :
+            raise Warning(_('Acceso Denegado: Esta usuario no tiene permiso para autorizar créditos, contacte a los(as) encargados(as) de Administración o Gerencia para que se autorice su crédito o cambie el "Plazo de pago" a "contado"'))
+
+class Sales(models.Model):
+    _inherit = "sale.order"
+    
+    def _payment_term_dynamic(self):
+        payment_term = self.env['account.payment.term'].sudo().search([('active','=',True),('company_id','=',self.env.user.company_id.id)])
+        vals = []
+        for item in payment_term:
+            if item.sudo().public:
+                vals.append((item.sudo().id,item.sudo().name + "*"))
+            else:
+                vals.append((item.sudo().id,item.sudo().name))
+        return vals
+    
+    payment_term = fields.Selection(selection=lambda self: self._payment_term_dynamic(),string="Plazos de pago(Editable)")
+    
+    
+    @api.onchange('payment_term')
+    def _onchange_payment_term(self):
+        self.payment_term_id = self.payment_term
+    
+    @api.model
+    def create(self,vals):
+        vals['payment_term_id'] = vals['payment_term']
+        res = super(Sales,self).create(vals)
+        return res
+    
+    def _exist_index(self,index,array):
+        try:
+            array[index]
+            return True
+        except :
+            return False
+    
+    
+    @api.model
+    def write(self,vals):
+        if self._exist_index('payment_term',vals):
+            vals['payment_term_id'] = vals['payment_term']
+        res = super(Sales,self).write(vals)
+        return res
+ 
