24e2099fb05f28504250659e859cb0a7cf4a58c0
Alejandro Zelaya <jandro.zg123@gmail.com>
2023-03-22 09:30:45 -0600

    ultimas actualizaciones

--- campos_referencia/__manifest__.py
+++ campos_referencia/__manifest__.py
@@ -18,12 +18,13 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base','stock'],
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
         "views/res_users_view.xml",
+        "views/stock_inventary.xml",
         "views/res_partner.xml"
     ],
     # only loaded in demonstration mode
