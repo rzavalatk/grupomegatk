44ef12391880fc4b7e73e300c17d56e63299f693
Alejandro Zelaya <jandro.zg123@gmail.com>
2023-12-09 12:05:10 -0600

    fields_megatk

--- fields_megatk/models/account_invoice.py
+++ fields_megatk/models/account_invoice.py
@@ -27,9 +27,33 @@ class Account_Move(models.Model):
 class AccountMoveLine(models.Model):
     _inherit = "account.move.line"
 
-    x_user_id = fields.Many2one('res.users', default=lambda self: self.env.user, string='Responsable')
+    x_user_id = fields.Many2one('res.users', compute='_compute_responsable_cotizacion', string='Responsable')
     obj_padre = fields.Many2one(related="move_id.invoice_user_id", string="ResponsableTem")
     x_series = fields.Text("Series")
+    
+    @api.depends('move_id.invoice_origin')
+    def _compute_responsable_cotizacion(self):
+        for line in self:
+            if line.move_id.invoice_origin and line.move_id.move_type == "out_invoice":
+                cotizacion = self.env['sale.order'].search([('name', '=', line.move_id.invoice_origin)], limit=1)
+                line.x_user_id = cotizacion.user_id if cotizacion.user_id else False
+                """for lines in cotizacion.order_line:
+                    line.x_user_id = lines.x_user_id if line.product_id.id == cotizacion.order_line.product_id.id else False"""
+            else:
+                line.x_user_id = self.env.user
+                
+    '''@api.depends('move_id.invoice_origin')
+    def _compute_serie(self):
+        for line in self:
+            if line.move_id.invoice_origin:
+                cotizacion = self.env['sale.order'].search([('name', '=', line.move_id.invoice_origin)], limit=1)
+                line.x_series = cotizacion.order_line.x_series if cotizacion.order_line.x_series else False
+                """for lines in cotizacion.order_line:
+                    line.x_series = lines.x_series if line.product_id.id == cotizacion.order_line.product_id.id else False"""
+            else:
+                line.x_series = "d"'''
+    
+    
     
     #date_maturity = fields.Date(string='Due Date', index=True, tracking=True, related='move_id.date_due',
     #    help="This field is used for payable and receivable journal entries. You can put the limit date for the payment of this line.")
