c87b90cf16730768f7be602742727dfa210b7c92
Alejandro Zelaya <jandro.zg123@gmail.com>
2024-01-29 15:10:17 -0600

    Sistema de tickets[nueva forma de contabilizar los tickets]

--- fields_megatk/models/account_invoice.py
+++ fields_megatk/models/account_invoice.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 from odoo import models, fields, api, _
 from odoo.exceptions import UserError
-
-import math
 import logging
 
 _logger = logging.getLogger(__name__)
@@ -57,12 +55,11 @@ class Account_Move(models.Model):
             if self.sorteo_id.fecha_inicio <= self.invoice_date <= self.sorteo_id.fecha_final:
                 
                 if self.amount_total > 1000:
-                    
-                    ticketsXcompra = math.floor(self.amount_total / 1000)
+                    tickets.append({'ticket': "ticket 1"})
                     
                     for fechas in self.sorteo_id.fechas_festivas:
                         if self.invoice_date == fechas.fecha:
-                            ticketsXcompra = ticketsXcompra * 2
+                            tickets.append({'ticket': "ticket 4"})
                             dia_festivo = True
                             break
                     
@@ -71,17 +68,26 @@ class Account_Move(models.Model):
                     for move_line in self.line_ids:
                         if not flag:
                             if move_line.product_id.marca_id.name == 'MAQUIRA':
-                                ticketsXcompra = ticketsXcompra * 2
+                                tickets.append({'ticket': "ticket 2"})
                                 #_logger.debug("Generando ticket 2")
                                 if self.x_student:
-                                    ticketsXcompra = ticketsXcompra * 3
-                                    
+                                    tickets.append({'ticket': "ticket 3"})
                                 flag = True
                                 
+                    
+                
+                
+            
+
                 # Limitar a un máximo de 3 tickets por compra si no es dia festivo
                 
-                for i in range(ticketsXcompra):
-                
+                if not dia_festivo:
+                    tickets = tickets[:3]
+                else:
+                    tickets = tickets[:4]
+
+                # Crear los registros de tickets
+                for ticket_data in tickets:
                     # Cambiar 'move_line_id' por 'name' o algún otro campo significativo
                     self.env['sorteo.ticket'].create({
                         'move_id': self.id,
