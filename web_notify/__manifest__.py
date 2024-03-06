1261a13add42bdf1c4883b686708d1fecae01b63
Alejandro Zelaya <jandro.zg123@gmail.com>
2024-03-06 14:25:51 -0600

    web_notify

--- web_notify/__manifest__.py
+++ web_notify/__manifest__.py
@@ -3,36 +3,20 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Web Notify',
-    'summary': """
+    "name": "Web Notify",
+    "summary": """
         Send notification messages to user""",
-    'version': '14.0.1.0.1',
-    'license': 'AGPL-3',
-    'author': 'ACSONE SA/NV,'
-              'AdaptiveCity,'
-              'Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/web',
-    'depends': [
-        'web',
-        'bus',
-        'base',
-    ],
-    'data': [
-        #'views/web_notify.xml'
-    ],
-    'demo': [
-        'views/res_users_demo.xml'
-    ],
-    
-    'qweb_template_dict': {
-        'backend': [
-            
-            '/web_notify/static/src/scss/webclient.scss',
-        ],
-        'frontend': [
-            '/web_notify/static/src/js/web_client.js',
-            
-        ],
+    "version": "16.0.1.0.1",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV," "AdaptiveCity," "Odoo Community Association (OCA)",
+    "development_status": "Production/Stable",
+    "website": "https://github.com/OCA/web",
+    "depends": ["web", "bus", "base", "mail"],
+    "assets": {
+        "web.assets_backend": [
+            "web_notify/static/src/js/services/*.js",
+        ]
     },
-    'installable': True,
+    "demo": ["views/res_users_demo.xml"],
+    "installable": True,
 }
