50f0aec7b70935164825879069890c7037bbf173
Alejandro Zelaya <jandro.zg123@gmail.com>
2024-03-25 13:49:17 -0600

    product.product

--- crons_mega/models/product.py
+++ crons_mega/models/product.py
@@ -5,12 +5,7 @@ class Products(models.TransientModel):
     _inherit = 'product.crons'
     
     
-    def update_product_ids(self):
-        products = self.env['product.product'].search([])
-        for product in products:
-            product.write({
-                'product_tmpl_id': product.id,
-            })
+   
 
 
 
