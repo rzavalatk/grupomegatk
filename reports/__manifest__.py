7ee3e5bdbad3e3d7f5e1272a2f4a311244d6c7c7
Alejandro Zelaya <jandro.zg123@gmail.com>
2024-05-09 10:24:17 -0600

    CRM / LEAD [campo name]

---
+++ reports/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'Control de reportes',
+    'version': '1.0',
+    'description': 'Modulo para control de reportes',
+    'summary': 'Modulo para control de reportes e imprimirlos',
+    'author': 'David Zuniga',
+    'license': 'LGPL-3',
+    'category': 'reports',
+    'depends': [
+        'base'
+    ],
+    'data': [
+        ''
+    ],
+    'auto_install': False,
+    'installable': True,
+    'application': True,
+    
+}
