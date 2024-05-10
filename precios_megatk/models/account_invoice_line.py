655e48a6f8581bba9e6b32363cb57ce2185b57ec
Alejandro Zelaya <jandro.zg123@gmail.com>
2024-05-10 13:59:59 -0600

    Precios mgtk

--- precios_megatk/models/account_invoice_line.py
+++ precios_megatk/models/account_invoice_line.py
@@ -21,7 +21,7 @@ class AccountMoveLine(models.Model):
     def _onchange_price_unit_product_id(self):
         if self.move_id.move_type == 'out_invoice':
             allowed_emails = [
-                'lvilleda@printexhn.net', 'rzavala@megatk.com', 'lmoran@megatk.com', 'kromero@megatk.com',
+                'lvilleda@printexhn.net', 'rzavala@megatk.com', 'lmoran@megatk.com', 'dvasquez@megatk.com',
                 'eduron@megatk.com', 'jmoran@meditekhn.com', 'msauceda@megatk.com', 'nfuentes@meditekhn.com'
             ]
             if self.env.user.email not in allowed_emails:
