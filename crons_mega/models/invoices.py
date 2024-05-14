64b2276be054061848f22ba726edc573eae7c27c
Alejandro Zelaya <jandro.zg123@gmail.com>
2024-05-14 11:44:27 -0600

    invoice expire mgtk

--- crons_mega/models/invoices.py
+++ crons_mega/models/invoices.py
@@ -33,8 +33,8 @@ class FacturasAVencer(models.Model):
     date = fields.Date("Fecha")
     company_id = fields.Many2one(
         "res.company", "Compañia", default=lambda self: self.env.user.company_id.id)
-    invoice_expire_line = fields.Many2many(
-        "invoice.expire.line", "Facturas")
+    invoice_expire_line = fields.One2many(
+        "invoice.expire.line", "invoice_expire_id", "Facturas")
     facturas_ids_customers = fields.One2many(
         "account.move", "expire_customer_id", "Facturas Vencidas")
     facturas_ids = fields.Many2many("account.move", "Facturas")
