16ef3b9a7ff9bb78d85b7c88be84feb1b9d7cc56
Alejandro Zelaya <jandro.zg123@gmail.com>
2024-05-16 14:22:38 -0600

    comisiones

--- comisiones/models/models.py
+++ comisiones/models/models.py
@@ -203,9 +203,10 @@ class Comisiones(models.Model):
             # denominador = len(line.invoice_line_id.invoice_id.payment_ids.ids)
             # promedio = promedio/denominador if denominador > 0 else 0
             # print("////////////",promedio,"/////////////")
-            #date_payment = []
+            date_payment = ''
             for item in line.invoice_line_id.move_id.invoice_payments_widget:
-                date_payment = item.date
+                _logger.warning('line : ' + item.date)
+                date_payment = str(item.date)
             if date_payment:
                 length = self.rangeDate(line.invoice_line_id.move_id.invoice_date_due,date_payment)
             else: 
