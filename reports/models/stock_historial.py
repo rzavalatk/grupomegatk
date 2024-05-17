161e1d359b7b13c1927895b6a7b6ddeaf85a255c
Alejandro Zelaya <jandro.zg123@gmail.com>
2024-05-17 14:04:55 -0600

    reports

--- reports/models/stock_historial.py
+++ reports/models/stock_historial.py
@@ -1,53 +1,67 @@
-
 from odoo import models, fields, api
-from odoo.exceptions import Warning
+from datetime import datetime
 
+class StockReportHistory(models.Model):
+    _name = 'stock.report.history'
+    _description = 'Stock Report History'
 
-class StockHistory(models.TransientModel):
-    _name = 'stock.history'
-    
-   #movimiento_ids = fields.Many2one('stock.quant', string='movimiento')
-    name = fields.Char('name')
-    #quant_id = fields.One2many('stock.quant', 'Quant')
-    product_id = fields.Many2one('product.product', 'Product')
-    location_id = fields.Many2one('stock.location', 'Location')
-    quantity = fields.Float('Quantity')
-    date = fields.Datetime('Date')
-    user_id = fields.Many2one('res.users', 'User')
+    name = fields.Char(string="Report Name", required=True)
+    date_from = fields.Date(string="Start Date", required=True)
+    date_to = fields.Date(string="End Date", required=True)
+    report_lines_from = fields.One2many('stock.report.line', 'report_id_from', string="Report Lines From", readonly=True)
+    report_lines_to = fields.One2many('stock.report.line', 'report_id_to', string="Report Lines To", readonly=True)
+    report_differences = fields.One2many('stock.report.difference', 'report_id', string="Report Differences", readonly=True)
 
-    @api.depends('quant_id')
-    def _compute_product_id(self):
-        for record in self:
-            if record.quant_id:
-                record.product_id = record.quant_id.product_id
+    def generate_reports(self):
+        self._generate_report_lines(self.date_from, 'report_lines_from')
+        self._generate_report_lines(self.date_to, 'report_lines_to')
+        self._calculate_differences()
 
-    @api.depends('quant_id')
-    def _compute_location_id(self):
-        for record in self:
-            if record.quant_id:
-                record.location_id = record.quant_id.location_id
+    def _generate_report_lines(self, date, field_name):
+        self.ensure_one()
+        StockQuant = self.env['stock.quant']
+        quants = StockQuant.search([('inventory_date', '<=', date)])
+        lines = []
+        for quant in quants:
+            lines.append((0, 0, {
+                'product_id': quant.product_id.id,
+                'quantity': quant.quantity,
+                'location_id': quant.location_id.id,
+            }))
+        self.write({field_name: lines})
 
-    @api.model
-    def create_history(self, quant_id, quantity):
-        self.env['stock.history'].create({
-            'quant_id': quant_id,
-            'product_id': quant_id.product_id,
-            'location_id': quant_id.location_id,
-            'quantity': quantity,
-            'date': fields.Datetime.now(),
-            'user_id': self.env.user.id,
-        })
+    def _calculate_differences(self):
+        self.ensure_one()
+        lines_from = {line.product_id.id: line for line in self.report_lines_from}
+        lines_to = {line.product_id.id: line for line in self.report_lines_to}
+        differences = []
+        for product_id in set(lines_from.keys()).union(lines_to.keys()):
+            qty_from = lines_from.get(product_id, {}).get('quantity', 0)
+            qty_to = lines_to.get(product_id, {}).get('quantity', 0)
+            differences.append((0, 0, {
+                'product_id': product_id,
+                'quantity_from': qty_from,
+                'quantity_to': qty_to,
+                'quantity_difference': qty_to - qty_from,
+            }))
+        self.report_differences = differences
 
+class StockReportLine(models.Model):
+    _name = 'stock.report.line'
+    _description = 'Stock Report Line'
 
-"""class StockQuant(models.Model):
-    _inherit = 'stock.quant'
+    report_id_from = fields.Many2one('stock.report.history', string="Report From", ondelete='cascade')
+    report_id_to = fields.Many2one('stock.report.history', string="Report To", ondelete='cascade')
+    product_id = fields.Many2one('product.product', string="Product", required=True)
+    quantity = fields.Float(string="Quantity", required=True)
+    location_id = fields.Many2one('stock.location', string="Location", required=True)
 
-    @api.model_update_fields()
-    def write(self, vals):
-        res = super(StockQuant, self).write(vals)
+class StockReportDifference(models.Model):
+    _name = 'stock.report.difference'
+    _description = 'Stock Report Difference'
 
-        if 'quantity' in vals:
-            for quant in self:
-                self.env['stock.history'].create_history(quant, quant.quantity)
-
-        return res"""
+    report_id = fields.Many2one('stock.report.history', string="Report", ondelete='cascade')
+    product_id = fields.Many2one('product.product', string="Product", required=True)
+    quantity_from = fields.Float(string="Quantity From", required=True)
+    quantity_to = fields.Float(string="Quantity To", required=True)
+    quantity_difference = fields.Float(string="Quantity Difference", required=True)
