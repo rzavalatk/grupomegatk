7da74cf4d1af9fe488b8746e9122df0fc5192edd
Alejandro Zelaya <jandro.zg123@gmail.com>
2024-05-21 14:34:47 -0600

    comisiones

--- comisiones/models/models.py
+++ comisiones/models/models.py
@@ -97,7 +97,7 @@ class Comisiones(models.Model):
 
                 self.name = user_id.name + " // " + str(self.date_init) + "::" + str(self.date_end)
         except :
-            self.name = self.company_id.name
+            self.name = self.company_id.name + " - " + self.type + " // " + self.date_init + "::" + self.date_end
             #self.name = "Error: verifique 'Compañia', 'Tipo', 'Fecha' o 'Usuarios'"
 
     name = fields.Char("Comisión", compute=_name_)
