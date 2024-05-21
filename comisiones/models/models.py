4d7014fad8d49a3e9d64bfdacc8709a475a14060
Alejandro Zelaya <jandro.zg123@gmail.com>
2024-05-21 14:38:33 -0600

    comisiones

--- comisiones/models/models.py
+++ comisiones/models/models.py
@@ -89,7 +89,7 @@ class Comisiones(models.Model):
     def _name_(self):
         try:
             if len(self.users_ids) > 1:
-                    self.name = self.company_id.name + " - " + self.type + " // " + self.date_init + "::" + self.date_end
+                    self.name = self.company_id.name + " // " + self.date_init + "::" + self.date_end
             else:
                 user_id = {}
                 for item in self.users_ids:
