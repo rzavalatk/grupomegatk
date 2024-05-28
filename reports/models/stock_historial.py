56a82fa47bf93c30453351d2ef85a43af8ddc16c
Alejandro Zelaya <jandro.zg123@gmail.com>
2024-05-28 14:21:52 -0600

    report

--- reports/models/stock_historial.py
+++ reports/models/stock_historial.py
@@ -37,7 +37,7 @@ class StockReportHistory(models.Model):
         #_logger.warning('Prueba reports : fecha='+ str(fecha_objeto))
         lines = []
         for quant in quants:
-            _logger.warning( quant.create_date)
+            _logger.warning( str(quant.create_date) + " // " + str(quant.product_id) + " // " + str(quant.quantity))
             """lines.append((0, 0, {
                 'product_id': quant.product_id.id,
                 'quantity': quant.product_qty,
