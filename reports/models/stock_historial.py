1780f397604608c20caf90577bce19572b898e76
Alejandro Zelaya <jandro.zg123@gmail.com>
2024-05-30 08:37:56 -0600

    report

--- reports/models/stock_historial.py
+++ reports/models/stock_historial.py
@@ -45,9 +45,9 @@ class StockReportHistory(models.Model):
             if products_groups:
                 for line_product in products_groups:
                     #product_id, quantity, date_create = dict(line_product).items()
-                    #_logger.warning( line_product['product_id']==quant.product_id.id )
-                    validacion = line_product['product_id']==quant.product_id.id
-                    if validacion:
+                    _logger.warning( line_product['product_id']==quant.product_id.id )
+                    
+                    if (line_product.values('product_id')==quant.product_id.id) is True:
                         line_product['quantity'], = line_product['quantity'], + quant.quantity
                     else:
                         products_groups.append((0,0, {
