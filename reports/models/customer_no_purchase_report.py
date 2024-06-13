2f50247c2e85cecc449cbc85baa902f01a419ce0
Alejandro Zelaya <jandro.zg123@gmail.com>
2024-06-13 08:53:19 -0600

    Modulo de reportes / reportes de clientes sin actividad

--- reports/models/customer_no_purchase_report.py
+++ reports/models/customer_no_purchase_report.py
@@ -29,7 +29,13 @@ class CustomerNoPurchaseReport(models.TransientModel):
             ('state', '=', 'posted'),
             ('move_type', '=', 'out_refund'),
         ]
-        account_orders = self.env['account.move'].search(domain)
+        account_orders = self.env['account.move'].search(['&', '&', '&', '&',
+            ('company_id', '=', self.company_id.id),
+            ('invoice_date', '>=', self.date_from),
+            ('invoice_date', '<=', self.date_to),
+            ('state', '=', 'posted'),
+            ('move_type', '=', 'out_refund'),
+        ])
         customer_ids = account_orders.mapped('partner_id.id')
         
         _logger.warning(len(account_orders))
