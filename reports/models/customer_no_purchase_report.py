fd0df2e395152764deec867215a28d801cd8eb3e
Alejandro Zelaya <jandro.zg123@gmail.com>
2024-06-13 09:01:07 -0600

    Modulo de reportes / reportes de clientes sin actividad

--- reports/models/customer_no_purchase_report.py
+++ reports/models/customer_no_purchase_report.py
@@ -39,8 +39,8 @@ class CustomerNoPurchaseReport(models.TransientModel):
         _logger.warning(len(account_orders))
         _logger.warning(len(customer_ids))
         
-        for partner in customer_ids:
-            _logger.warning(partner.name)
+        for partner in account_orders:
+            _logger.warning(partner)
         
 
         domain_customers = [
