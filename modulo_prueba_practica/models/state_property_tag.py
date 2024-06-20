5af5f3beec8df0aaf1e6deb7512ea2f575e4374e
Jiovanny <azelaya@megatk.com>
2024-06-20 14:59:22 -0600

    etiquetas

--- modulo_prueba_practica/models/state_property_tag.py
+++ modulo_prueba_practica/models/state_property_tag.py
@@ -1,6 +1,6 @@
-from odoo import models, fields
+from odoo import models,fields
 
-class etiquetas(models, fields):
+class etiquetas(models.fields):
     _name = "tag.property"
     _description = "Etiquetas"
 
