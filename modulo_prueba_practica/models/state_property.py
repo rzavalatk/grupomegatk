25c8edf80dac999054e036b9f82ba63e08c5f864
Jiovanny <azelaya@megatk.com>
2024-06-21 15:32:30 -0600

    etiquetas

--- modulo_prueba_practica/models/state_property.py
+++ modulo_prueba_practica/models/state_property.py
@@ -10,6 +10,7 @@ class TestModel(models.Model):
         fecha_futura = fecha_actual + timedelta(days=30 * 3)
         return fecha_futura.strftime("%Y-%m-%d")  
     
+    
     name = fields.Char('Nombre', required=True)
     description = fields.Text('Descripcion')
     postcode = fields.Char('Codigo postal')
