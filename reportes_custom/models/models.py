cd0f05a4de81568f174ab1ecc8f8df1f968e56b7
Alejandro Zelaya <jandro.zg123@gmail.com>
2024-06-25 10:22:32 -0600

    MOdulo de reportes / facturas proforma y el con del amount_text

--- reportes_custom/models/models.py
+++ reportes_custom/models/models.py
@@ -1,7 +1,21 @@
 # -*- coding: utf-8 -*-
-
-from odoo import models, api, fields
+import logging
+import math
 import base64
+from lxml import etree
+
+from odoo import api, fields, models, tools, _
+from odoo.exceptions import UserError
+from odoo.tools import parse_date
+
+_logger = logging.getLogger(__name__)
+
+
+try:
+    from num2words import num2words
+except ImportError:
+    _logger.warning("The num2words python library is not installed, amount-to-text features won't be fully available.")
+    num2words = None
 
 
 class StockPicking(models.Model):
@@ -46,6 +60,40 @@ class StockPicking(models.Model):
 
     sign = fields.Binary()
     passed = fields.Char(string="Aprobado", default="No")
+
+class ResCurrencyInherit(models.Model):
+    _inherit = 'res.currency'
+
+    def amount_to_text(self, amount):
+        self.ensure_one()
+        
+        def _num2words(number, lang):
+            try:
+                return num2words(number, lang=lang).title()
+            except NotImplementedError:
+                return num2words(number, lang='en').title()
+
+        if num2words is None:
+            logging.getLogger(__name__).warning("The library 'num2words' is missing, cannot render textual amounts.")
+            return ""
+
+        formatted = "%.{0}f".format(self.decimal_places) % amount
+        parts = formatted.partition('.')
+        integer_value = int(parts[0])
+        fractional_value = int(parts[2] or 0)
+
+        lang = tools.get_lang(self.env)
+        amount_words = tools.ustr('{amt_value} {amt_word}').format(
+                        amt_value=_num2words(integer_value, lang=lang.iso_code),
+                        amt_word=self.currency_unit_label,
+                        )
+        if not self.is_zero(amount - integer_value):
+            amount_words += ' ' + _('con') + tools.ustr(' {amt_value} {amt_word}').format(
+                        amt_value=_num2words(fractional_value, lang=lang.iso_code),
+                        amt_word=self.currency_subunit_label,
+                        )
+        return amount_words
+
     
 class InvoiceOrder(models.Model):
     _inherit = 'account.move'
