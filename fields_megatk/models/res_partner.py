c468a10d90810927ae12dabd50949a54f414335d
Alejandro Zelaya <jandro.zg123@gmail.com>
2024-06-26 16:33:42 -0600

    Se añadio al metodo create de res.partner la condicion de que busque el RTN si ya existe

--- fields_megatk/models/res_partner.py
+++ fields_megatk/models/res_partner.py
@@ -11,3 +11,14 @@ class Campos_clientes(models.Model):
     
     x_customer = fields.Boolean(string='Es cliente ', default=False)
     x_supplier = fields.Boolean(string='Es proveedor', default=False)
+    
+    @api.model_create_multi
+    def create(self, vals_list):
+        """Condicion que busca el vat de todos los clientes y luego verifica si ya existe."""
+        
+        partner = self.env['res.partner'].search([('vat', '=', self.vat)])
+        
+        if partner:
+            raise UserError(_("Usuario ya creado con este RTN / En caso de duplicar este contacto con un numero diferente de RTN se le multara."))
+        
+        return super().create(vals_list)
