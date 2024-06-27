7247776b548f4dd87b8652f955c8ba961cb00594
Jiovanny <azelaya@megatk.com>
2024-06-27 16:20:17 -0600

    ofertas page

--- modulo_prueba_practica/models/__init__.py
+++ modulo_prueba_practica/models/__init__.py
@@ -2,4 +2,4 @@
 from . import state_property
 from . import state_property_type
 from . import state_property_tag
-#from . import state_property_offer
+from . import state_property_offer
--- modulo_prueba_practica/models/state_property_offer.py
+++ modulo_prueba_practica/models/state_property_offer.py
@@ -1,13 +1,13 @@
-from odoo import models, fields
+from odoo import models,fields
 
 class ofertas(models.Model):
     _name = "offers.property"
     _description = "Oferta"
 
     price = fields.Float('Precio')
-    status = fields.Selection(copy = False[
+    status = fields.Selection([
         ('accepted', 'Aceptado'),
         ('refused','Rechazado')
     ], string='Estatus')
-   # partner_identification = fields.Many2one('res.partner', string='Identificacion de socio', required=True)
-    #property_id = fields.Many2one('test.model', string='ID de propiedad', required=True)
+    partner_identification = fields.Many2one('res.partner', string='Identificacion de socio', required=True)
+    property_id = fields.Many2one('test.model', string='ID de propiedad', required=True)
