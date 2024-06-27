742003b9d28735615c2eb77a70a05a2ed4eff04d
Alejandro Zelaya <jandro.zg123@gmail.com>
2024-06-27 11:52:42 -0600

    fields_megatk // res.partner // 1

--- conciliacion_bancaria/models/conciliacion.py
+++ conciliacion_bancaria/models/conciliacion.py
@@ -115,7 +115,7 @@ class ConciliacionBancaria(models.Model):
         for movimiento in obj_move_id:
             obj_line = self.env["conicliacion.bancaria.line"]
             for obj_line_concil_delete in self.conciliacion_line:
-                if obj_line_concil_delete.move_id.state == 'draft' or not obj_line_concil_delete.move_id:
+                if obj_line_concil_delete.move_id.state == 'draft' or obj_line_concil_delete.move_id.state == 'cancel' or not obj_line_concil_delete.move_id:
                     obj_line_concil_delete.unlink()
 
             obj_line_concil_line = self.env["conicliacion.bancaria.line"].search([('conciliacion_id', '=', self.id), ('move_line_id', '=', movimiento.id)])
