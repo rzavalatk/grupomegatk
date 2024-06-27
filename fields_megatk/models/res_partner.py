34252f9d627f53b151ab89e7cc657068ab97161d
Alejandro Zelaya <jandro.zg123@gmail.com>
2024-06-27 09:37:09 -0600

    Cambios a proforma y cotizacion en el footer que salia la direccion del usuario

--- fields_megatk/models/res_partner.py
+++ fields_megatk/models/res_partner.py
@@ -14,13 +14,13 @@ class Campos_clientes(models.Model):
     
     #x_clientes_varios = fields.Boolean(string='Clientes varios', default=False)
     
-    @api.model_create_multi
+    """@api.model_create_multi
     def create(self, vals_list):
-        """Condicion que busca el vat de todos los clientes y luego verifica si ya existe."""
+        #Condicion que busca el vat de todos los clientes y luego verifica si ya existe.
         
         partner = self.env['res.partner'].search([('vat', '=', self.vat)])
         
         if partner:
             raise UserError(_("Usuario ya creado con este RTN / En caso de duplicar este contacto con un numero diferente de RTN se le multara."))
         
-        return super().create(vals_list)
+        return super().create(vals_list)"""
