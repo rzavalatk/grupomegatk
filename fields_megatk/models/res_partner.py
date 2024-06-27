e31285a79700a65d52f894e42763d6050ef8c293
Alejandro Zelaya <jandro.zg123@gmail.com>
2024-06-27 10:59:46 -0600

    fields_megatk // res.partner // 1

--- fields_megatk/models/res_partner.py
+++ fields_megatk/models/res_partner.py
@@ -10,6 +10,8 @@ _logger = logging.getLogger(__name__)
 #CAMPOS EN FORMULARIO CONTACTO/VENTAS Y COMPRAS/VARIOS
 class Campos_clientes(models.Model):
     _inherit = "res.partner"
+    
+    encontrado = False
 
     x_zonac = fields.Selection([('centro','Centro (Teg, Comayagua, Sigua)'),('norte','Norte (SPS, Pto Cortez, Ceiba)')
     	,('oriente','Oriente (Danli y El Paraiso)'),('sur','Sur (Choluteca, San Lor, Amap)')],string = 'Zona cliente')
@@ -18,19 +20,22 @@ class Campos_clientes(models.Model):
     x_customer = fields.Boolean(string='Es cliente ', default=False)
     x_supplier = fields.Boolean(string='Es proveedor', default=False)
     
+    @api.onchange("vat")
+    def onchangevay(self):         
+        
+        partner = self.env['res.partner'].search([('vat', '=', self.vat)])
+        
+        if partner:
+            self.encontrado = True
     
     
     @api.model_create_multi
     def create(self, vals_list):
         """Condicion que busca el vat de todos los clientes y luego verifica si ya existe."""
+  
+        _logger.warning("Nombre del contacto encontrado: " + self.encontrado)
         
-        for partner_id in self:
+        if self.encontrado:
+            raise UserError(_("Usuario ya creado con este RTN / En caso de duplicar este contacto con un numero diferente de RTN se le multara."))
         
-            partner = self.env['res.partner'].search([('vat', '=', partner_id.vat)])
-            
-            _logger.warning("Nombre del contacto encontrado: " + partner.name)
-            
-            if partner:
-                raise UserError(_("Usuario ya creado con este RTN / En caso de duplicar este contacto con un numero diferente de RTN se le multara."))
-            
-            return super().create(vals_list)
+        return super().create(vals_list)
