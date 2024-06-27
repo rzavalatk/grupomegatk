330ac3263614291407dae27226168599698ab89e
Alejandro Zelaya <jandro.zg123@gmail.com>
2024-06-27 08:35:05 -0600

    fields_megatk // res.partner // 1

--- fields_megatk/models/res_partner.py
+++ fields_megatk/models/res_partner.py
@@ -12,6 +12,8 @@ class Campos_clientes(models.Model):
     x_customer = fields.Boolean(string='Es cliente ', default=False)
     x_supplier = fields.Boolean(string='Es proveedor', default=False)
     
+    x_clientes_varios = fields.Boolean(string='Clientes varios', default=False)
+    
     @api.model_create_multi
     def create(self, vals_list):
         """Condicion que busca el vat de todos los clientes y luego verifica si ya existe."""
@@ -19,6 +21,6 @@ class Campos_clientes(models.Model):
         partner = self.env['res.partner'].search([('vat', '=', self.vat)])
         
         if partner:
-            raise UserError(_("Usuario ya creado."))
+            raise UserError(_("Usuario ya creado con este RTN / En caso de duplicar este contacto con un numero diferente de RTN se le multara."))
         
         return super().create(vals_list)
