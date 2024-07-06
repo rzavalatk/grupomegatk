d3dff772f22bfb656b2d42fcc8ae965f729c4d98
Alejandro Zelaya <jandro.zg123@gmail.com>
2024-07-06 11:24:53 -0600

    notificar 5 dias antes de vencimiento de factura

--- crons_mega/models/invoices.py
+++ crons_mega/models/invoices.py
@@ -3,6 +3,12 @@ from odoo import models, fields, api
 from datetime import datetime, timedelta
 import pytz
 
+import logging
+import math
+
+
+_logger = logging.getLogger(__name__)
+
 class AccountMove(models.Model):
     _inherit = 'account.move'
 
@@ -14,9 +20,15 @@ class AccountMove(models.Model):
         due_date = today + timedelta(days=5)
         invoices = self.search([('invoice_date_due', '=', due_date), ('state', '=', 'posted')])
         mail_template = self.env.ref('crons_mega.mail_template_notification_invoice_due')
+        
+        _logger.warning('Fecha de vencimiento : ' + str(due_date))
 
+        
         for invoice in invoices:
+            _logger.warning('Numero de factura : ' + str(invoice.number))
             if invoice.invoice_user_id:
-                mail_template.sudo().send_mail(invoice.id, force_send=True)
+                _logger.warning('Fecha de vencimiento : ' + str(invoice.invoice_user_id.login))
+                mail_template.sudo().send_mail(invoice.id, email_values={'email_to': invoice.invoice_user_id.login}, force_send=True)
                 if invoice.partner_id.email:
+                    _logger.warning('Fecha de vencimiento : ' + str(invoice.partner_id.email))
                     mail_template.sudo().send_mail(invoice.id, email_values={'email_to': invoice.partner_id.email}, force_send=True)
