709319ce82c30d41ce7b23c74d4ea9a31aca3243
Alejandro Zelaya <jandro.zg123@gmail.com>
2024-07-11 10:57:23 -0600

    se agrego restricción a los vendedores para no crear productos

--- restringir_vendedor/models/models.py
+++ restringir_vendedor/models/models.py
@@ -13,10 +13,44 @@ class Users(models.Model):
 class Leads(models.Model):
     _inherit = "crm.lead"
     
-    #@api.multi
+    @api.model_create_multi
     def unlink(self):
         if self.env.user.vendor:
             raise Warning("No tiene permisos para realizar esta acción. Consulte con su Administrador.")
         else:
             return super(Leads, self).unlink()
+        
+class ProductNoCreate(models.Model):
+    _inherit = "product.product"
+    
+    @api.model_create_multi
+    def create(self, vals_list):
+        if self.env.user.vendor:
+            raise Warning("No tiene permisos para realizar esta acción. Consulte con su Administrador.")
+        else:
+            return super(ProductNoCreate, self).create(vals_list)
+        
+    @api.model_create_multi
+    def write(self, vals):
+        if self.env.user.vendor:
+            raise Warning("No tiene permisos para realizar esta acción. Consulte con su Administrador.")
+        else:
+            return super(ProductNoCreate, self).write(vals)
+        
+class ProductTemplateNoCreate(models.Model):
+    _inherit = "product.template"
+    
+    @api.model_create_multi
+    def create(self, vals_list):
+        if self.env.user.vendor:
+            raise Warning("No tiene permisos para realizar esta acción. Consulte con su Administrador.")
+        else:
+            return super(ProductTemplateNoCreate, self).create(vals_list)
+        
+    @api.model_create_multi
+    def write(self, vals):
+        if self.env.user.vendor:
+            raise Warning("No tiene permisos para realizar esta acción. Consulte con su Administrador.")
+        else:
+            return super(ProductTemplateNoCreate, self).write(vals)
     
