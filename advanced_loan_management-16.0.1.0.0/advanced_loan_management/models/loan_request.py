dc8daf685be4f9dfbc1ad4fb29bc5d866aef9897
Alejandro Zelaya <jandro.zg123@gmail.com>
2024-07-29 11:00:51 -0600

    Prestamos

--- advanced_loan_management-16.0.1.0.0/advanced_loan_management/models/loan_request.py
+++ advanced_loan_management-16.0.1.0.0/advanced_loan_management/models/loan_request.py
@@ -18,14 +18,7 @@ class Prestamo(models.Model):
     _inherit = ['mail.thread']
     _description = 'Modelo de Préstamo'
 
-    #Datos generales
-    name = fields.Char(string='Número de Préstamo', required=True, copy=False, readonly=True, default='Nuevo')
-    partner_id = fields.Many2one('res.partner', string='Cliente', required=True, readonly=True, states={'borrador': [('readonly', False)]}, copy=False)
-    remaining_capital = fields.Monetary('Capital restante', readonly=True,  copy=False,) #Se tiene que crear metodo computado para la asignación constante de cuanto capital queda
-    pay_capital = fields.Monetary('Capital pagado',  readonly=True, states={'borrador': [('readonly', False)]}, copy=False,)
-    note = fields.Text('Notas', readonly=True, states={'borrador': [('readonly', False)]}, copy=False) #Agregar a un campo en una page del notebook
-    disbursal_amount = fields.Float(string="Disbursal_amount",
-                                    help="Total loan amount available to disburse")
+    
     
     #Datos del prestamo
     amount_borrowed = fields.Monetary(string='Monto del Préstamo', store=True, readonly=True, states={'borrador': [('readonly', False)]},)
