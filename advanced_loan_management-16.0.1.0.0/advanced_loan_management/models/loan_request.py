1a286c1ba538164d22aae7f9a78aafbd957798a2
Alejandro Zelaya <jandro.zg123@gmail.com>
2024-07-29 11:04:04 -0600

    Prestamos

--- advanced_loan_management-16.0.1.0.0/advanced_loan_management/models/loan_request.py
+++ advanced_loan_management-16.0.1.0.0/advanced_loan_management/models/loan_request.py
@@ -49,53 +49,18 @@ class Prestamo(models.Model):
     date_init = fields.Date(string='Fecha de Inicio', required=True, default=lambda self: date.today(), readonly=True, states={'borrador': [('readonly', False)]},) #SE TIENE QUE CALCULAR AUTOMATICO CUANDO SE ELIJE DURACION
     date_ends = fields.Date(string='Fecha final', compute='_compute_date_ends', store=True)
     
+    #Datos de cuentas bancarias
+    company_id = fields.Many2one('res.company', string='Company', change_default=True, required=True, default=lambda self: self.env.user.company_id, readonly=True, states={'borrador': [('readonly', False)]},)
+    recibir_pagos = fields.Many2one("account.journal", "Recibir pagos",  domain=[('type', '=', 'bank')], required=True,)
+    account_id = fields.Many2one('account.account', 'Cuenta de intereses', required=True)
+    account_int_moratorio = fields.Many2one('account.account', 'Cuenta de intereses moratorios', required=True)
+    account_gasto_id = fields.Many2one('account.account', 'Cuenta de gastos', required=True, readonly=True, states={'borrador': [('readonly', False)]},)
+    user_id = fields.Many2one('res.users', string='Responsable', index=True, default=lambda self: self.env.user, readonly=True, states={'draft': [('readonly', False)]},)
+    debit_account_id = fields.Many2one('account.account', string="Cuenta de debito", help="Elija cuenta para débito por desembolso")
+    credit_account_id = fields.Many2one('account.account', string="Cuenta de credito", help="Elija cuenta para credito por desembolso")
     
     
-    #Datos de contabilidad
-    payment_term_id = fields.Many2one('account.payment.term', string='Plazo de pago',required=True, readonly=True, states={'borrador': [('readonly', False)]},)
-    interest_rate = fields.Integer(string='Tasa de Interés', required=True, readonly=True, states={'borrador': [('readonly', False)]},)
-    currency_id = fields.Many2one('res.currency', 'Moneda', readonly=True, states={'borrador': [('readonly', False)]}, default=lambda self: self.env.user.company_id.currency_id)
-    gasto_prestamo = fields.Monetary(string='Gastos administrativos', default=0, readonly=True, states={'borrador': [('readonly', False)]}, copy=False,)
-    
-    #Variables de conteo
-    invoice_count_cxc = fields.Integer(string='Factura Count', compute='_compute_invoiced', readonly=True)
-    payment_count = fields.Integer(string='Payment Count', compute='_compute_invoiced', readonly=True)
-    cuotas_count = fields.Integer(string='cuotas Count', compute='_compute_invoiced', readonly=True)
-    invoice_cxc_ids = fields.Many2many("account.move", string='Facturas cxc', readonly=True, copy=False)
-    payment_ids = fields.Many2many("account.payment", string="Pagos", copy=False,)
-       
-    payment_frequency = fields.Selection([
-        ('365', 'Diario'),
-        ('52', 'Semanal'),
-        ('24', 'Quincenal'),
-        ('12', 'Mensual'),
-        ('6', 'Bimestral'),
-        ('4', 'Trimestral'),
-        ('1', 'Anual')
-    ], string='Frecuencia de Pago', default='12', required=True, readonly=True, states={'borrador': [('readonly', False)]},)
-    
-    loan_type = fields.Selection([
-        ('personal', 'Personal'),
-        ('financiamiento', 'Financiamiento')
-    ], string='Tipo de Préstamo', default="personal", required=True, readonly=True, states={'borrador': [('readonly', False)]},)
-    
-    state = fields.Selection([
-        ('borrador', 'Borrador'),
-        ('generado', 'Generado'),
-        ('confirmado', 'Confirmado'),
-        ('aprobado', 'Aprobado'),
-        ('pro_pago', 'Proceso de pago')
-        ('rechazado', 'Rechazado'),
-        ('cancelado', 'Cancelado'),
-        ('pagado', 'Pagado')
-    ], string='Estado', default='borrador', required=True)
-       
-    
-    quota_ids = fields.One2many('cuota', 'prestamo_id', string='Cuotas', readonly=True)
-    #contrato_id = fields.Many2one('contrato', string='Contrato')
-    #garantia_ids = fields.One2many('garantia', 'prestamo_id', string='Garantías')
-
-    
+        
     """
     |||||||| METODOS COMPUTADOS PARA ASIGNACION DE VALORES ||||||||||
         
