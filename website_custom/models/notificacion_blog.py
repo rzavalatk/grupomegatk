9a7d3157172614bdd496affbd806eb33fdb419d0
Jiovanny <azelaya@megatk.com>
2024-08-01 15:03:05 -0600

    correccion de URL en notificacion de nuevo blog

--- website_custom/models/notificacion_blog.py
+++ website_custom/models/notificacion_blog.py
@@ -13,7 +13,7 @@ class notificion_blog(models.Model):
         if vals.get('is_published'):
             for post in self.filtered(lambda p: p.active):
                 post.blog_id.message_post_with_view(
-                    'nuevo_blog_publicado',
+                    'nuevo_blog_aviso',
                     subject=post.name,
                     values={'post': post},
                     subtype_id=self.env['ir.model.data']._xmlid_to_res_id('website_blog.mt_blog_blog_published'))
