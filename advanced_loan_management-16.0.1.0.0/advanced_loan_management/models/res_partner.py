95ccff990ed7e28e8eecc89e17a8d0a9bde8987c
Alejandro Zelaya <jandro.zg123@gmail.com>
2024-08-08 09:30:53 -0600

    cuota

--- advanced_loan_management-16.0.1.0.0/advanced_loan_management/models/res_partner.py
+++ advanced_loan_management-16.0.1.0.0/advanced_loan_management/models/res_partner.py
@@ -9,7 +9,7 @@ class ResPartner(models.Model):
 
     def _compute_partner_loans(self):
         """Calcula el importe del préstamo y el número total de préstamos de un socio.."""
-        self.loan_count = self.env['prestamo'].search_count(
+        self.loan_count = self.env['loan.request'].search_count(
             [('partner_id', '=', self.id),
              ('state', 'in', ('pro_pago', 'pagado'))])
 
@@ -24,7 +24,7 @@ class ResPartner(models.Model):
             'type': 'ir.actions.act_window',
             'name': 'Loans',
             'view_mode': 'tree',
-            'res_model': 'prestamo',
+            'res_model': 'loan.request',
             'domain': [('partner_id', '=', self.id)],
             'context': "{'create': False}"
         }
