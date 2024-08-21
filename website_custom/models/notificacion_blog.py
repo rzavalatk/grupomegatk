810525d1971a3d5d91173889a26268d9eed9ecf2
jmorales <jmorales@megatk.com>
2024-08-21 15:50:29 -0600

    prueba redireccionamiento de blog

--- website_custom/models/notificacion_blog.py
+++ website_custom/models/notificacion_blog.py
@@ -1,22 +1,11 @@
-from odoo import models, fields, api
 
-class notificacion_blog(models.Model):
-    _inherit = ['blog.blog']
-
-    def _check_for_publication(self, vals):
+def _check_for_publication(self, vals):
         if vals.get('is_published'):
             for post in self.filtered(lambda p: p.active):
-                # Obtenemos el dominio del sitio web asociado al post
-                website_domain = post.website_id.domain
-
-                # Construimos la URL completa del post
-                url = f"https://{website_domain}/blog/#{slug(object)}/#{slug(post)}"
-
-                # Enviamos un correo con el enlace correcto
                 post.blog_id.message_post_with_view(
-                    'website_blog.blog_post_template_new_post_blog',
+                    'website_blog.blog_post_template_new_post',
                     subject=post.name,
-                    values={'post': post, 'url': url},
+                    values={'post': post},
                     subtype_id=self.env['ir.model.data']._xmlid_to_res_id('website_blog.mt_blog_blog_published'))
             return True
         return False
