ecc29810b521fa5721797febf9f7378d7be8fb27
jmorales <jmorales@megatk.com>
2024-08-22 13:50:04 -0600

    prueba redireccionamiento de blog

--- website_custom/models/notificacion_blog.py
+++ website_custom/models/notificacion_blog.py
@@ -3,7 +3,7 @@ def _check_for_publication(self, vals):
         if vals.get('is_published'):
             for post in self.filtered(lambda p: p.active):
                 post.blog_id.message_post_with_view(
-                    'website_blog.blog_post_template_new_post',
+                    'website_blog.blog_nuevo_post_mail',
                     subject=post.name,
                     values={'post': post},
                     subtype_id=self.env['ir.model.data']._xmlid_to_res_id('website_blog.mt_blog_blog_published'))
