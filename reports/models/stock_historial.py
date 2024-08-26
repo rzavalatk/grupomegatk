53a75e248ddebde0629d667e9a7df2d14adda559
Alejandro Zelaya <jandro.zg123@gmail.com>
2024-08-26 07:35:05 -0600

    Agregar linea y marca al reporte excel

--- reports/models/stock_historial.py
+++ reports/models/stock_historial.py
@@ -172,9 +172,9 @@ class StockReportHistory(models.Model):
         col_widths_lines_reports = [45, 20]  # Ajusta estos valores según sea necesario
         formatos_lines_reports = [None, number_format]  # Formatos para cada columna
         
-        encabezados_reports_differences = ['Producto', 'Cantidad inicial', 'Cantidad final', 'Movimiento', 'Precio de coste', 'Precio de venta']
-        col_widths_reports_differences = [45, 25, 25, 25, 25, 25]  # Ajusta estos valores según sea necesario
-        formatos_reports_differences = [None, number_format, number_format, number_format, currency_format, currency_format]  # Formatos para cada columna
+        encabezados_reports_differences = ['Producto', 'Cantidad inicial', 'Cantidad final', 'Movimiento', 'Precio de coste', 'Precio de venta', 'Linea', 'Marca']
+        col_widths_reports_differences = [45, 25, 25, 25, 25, 25, 30, 30]  # Ajusta estos valores según sea necesario
+        formatos_reports_differences = [None, number_format, number_format, number_format, currency_format, currency_format, None, None]  # Formatos para cada columna
         
         # Preparar los datos
         datos_lines_from_report = [
@@ -201,6 +201,8 @@ class StockReportHistory(models.Model):
                 record.quantity_difference,
                 record.standard_price,
                 record.lst_price,
+                record.linea,
+                record.marca
             )
             for record in self.report_differences
         ]
