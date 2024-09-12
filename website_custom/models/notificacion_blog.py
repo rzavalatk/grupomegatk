eefb2b20f737e0c30b15541b25ce7c2355080ce4
jmorales <jmorales@megatk.com>
2024-09-12 07:56:11 -0600

    blog

--- website_custom/models/notificacion_blog.py
+++ website_custom/models/notificacion_blog.py
@@ -21,7 +21,7 @@ class BlogPost(models.Model):
         if vals.get('is_published'):
             for post in self.filtered(lambda p: p.active):
                 post.blog_id.message_post_with_view(
-                    'website_blog.blog_post_template_new_post',
+                    'website_blog.blog_new_mail_post',
                     subject=post.name,
                     values={'post': post},
                     subtype_id=self.env['ir.model.data']._xmlid_to_res_id('website_blog.mt_blog_blog_published'))
