e0968caba32a48e2a5294cbcf1bcf27d3f558012
jmorales <jmorales@megatk.com>
2024-09-17 11:10:05 -0600

    agregar modulo sar a menu de configuracion de pos

--- pos-master/sar_pos/models/__init__.py
+++ pos-master/sar_pos/models/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 ##############################################################################
-from . import account_invoice
+#from . import account_invoice
 from . import account
 from . import fiscal_sequence
 from . import ir_sequence
