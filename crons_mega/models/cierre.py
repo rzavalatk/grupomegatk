d3ebe0817aa737d8e516e7451530454763d45ceb
Alejandro Zelaya <jandro.zg123@gmail.com>
2024-09-24 15:57:07 -0600

    Modelo de cierre diario

--- crons_mega/models/cierre.py
+++ crons_mega/models/cierre.py
@@ -613,7 +613,7 @@ class CierreDiario(models.Model):
                     })
                     ids.append(obj.id)"""
             for i in ids:
-                principal_emails = "dzuniga@megatk.com,dvasquez@megatk.com"
+                principal_emails = "lmoran@megatk.com,jmoran@meditekhn.com,dvasquez@megatk.com"
                 cc_mega = "yalvarado@megatk.com"
                 cc_meditek = "nfuentes@meditekhn.com"
                 cierre = self.sudo().browse(i)
@@ -632,10 +632,10 @@ class CierreDiario(models.Model):
                         cc_mega += ",vmoran@megatk.com"
                         cc_meditek += "dgarcia@meditekhn.com"
                     # print("/////////////",principal_emails,cc_mega,"//////////////")
-                    cierre.send_email(principal_emails )
+                    cierre.send_email(principal_emails, cc_mega)
                 if cierre.company_id.sudo().id in [9]:
                     time.sleep(1)
-                    cierre.send_email(principal_emails)
+                    cierre.send_email(principal_emails, cc_meditek)
                 time.sleep(1)
 
     def go_to_view_tree(self):
