b13b501be1d27007d33b31c77c5603d2d48a1a97
jmorales <jmorales@megatk.com>
2024-10-08 09:16:54 -0600

    sar menu

--- sar_pos/__manifest__.py
+++ sar_pos/__manifest__.py
@@ -2,28 +2,28 @@
 ##############################################################################
 
 {
-    'name': "Facturación Electrónica Honduras-2024",
+    'name': "Facturación Electrónica Honduras",
     'summary': """
         Regulación del SAR para regimene de facturación para autoimpresores
         """,
     'description': """
          Regulación del SAR para regimene de facturación para autoimpresores
     """,
-    'author': 'Romel Zavala',
+    'author': 'César Alejandro Rodriguez.',
     'version': '1.0',
     'license': 'Other proprietary',
     'maintainer': '',
     'contributors': '',
     'category': 'Extra Tools',
-    'depends': ['base'],
+    'depends': ['base', 'account'],
     'data': [
-        'views/account_invoice_view.xml',
-        'views/config_authorization_code_view.xml',
-        'views/config_journal_view.xml',
-        'views/ir_sequence_view.xml',
-        'security/groups.xml',
-        'security/ir.model.access.csv',
-        'wizard/journal_settings_view.xml',
+        "security/groups.xml",
+        "security/ir.model.access.csv",
+        "wizard/journal_settings_view.xml",
+        "views/config_journal_view.xml",
+        "views/config_authorization_code_view.xml",
+        "views/ir_sequence_view.xml",
+        "views/account_invoice_view.xml",
     ],
     'demo': [
     ],
