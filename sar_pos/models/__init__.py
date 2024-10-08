fd7f40026001ac7afbbd5d770a124a3a2e086f9a
jmorales <jmorales@megatk.com>
2024-10-08 08:54:08 -0600

    sar menu

--- sar_pos/models/__init__.py
+++ sar_pos/models/__init__.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 ##############################################################################
-#from . import account_invoice
-#from . import account
-#from . import fiscal_sequence
+from . import account_invoice
+from . import account
+from . import fiscal_sequence
 from . import ir_sequence
 
 
