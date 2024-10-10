d2a829bc04ad5347407872d2652f5b46995717df
jmorales <jmorales@megatk.com>
2024-10-10 08:13:21 -0600

    modulo para descartar visitas de bots website

---
+++ filtro_de_bots_website/controllers/__init__.py
@@ -0,0 +1,7 @@
+##############################################################################
+# Copyright (c) 2022 lumitec GmbH (https://www.lumitec.solutions)
+# All Right Reserved
+#
+# See LICENSE file for full licensing details.
+##############################################################################
+from . import scroll_event
