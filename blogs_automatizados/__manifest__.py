47105bd537f50b2eba2708f85853a228b63c3a6e
jmorales <jmorales@megatk.com>
2024-11-01 11:54:33 -0600

    modulo para automatizar la publicacion de blog

---
+++ blogs_automatizados/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Automatizar publicacion de blogs',
+    'version': '1.0',
+    'category': 'Blog',
+    'author': 'Jiovanny, ',
+    'data': [
+        'views/social_media_post_view.xml',
+    ],
+    'application': False,
+    'installable': True,
+}
