23d17b460cc3f32e78ffd23ddf69a1739a670a1c
Alejandro Zelaya <jandro.zg123@gmail.com>
2024-11-20 07:51:19 -0600

    Modulo de prestamos

--- prestamos_financieros/models/__init__.py
+++ prestamos_financieros/models/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from . import loan_type, loan_documents, loan_request, repayment_lines, \
-    res_config_settings, account_move, res_partner, account_payment_register
+    account_move, res_partner, account_payment_register
 
 
 
