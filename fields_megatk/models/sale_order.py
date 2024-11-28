11eb0da7c7ddaf840bd4661367ef4ddd307a48d3
Alejandro Zelaya <jandro.zg123@gmail.com>
2024-11-28 13:23:26 -0600

    Precio unitario, dos digitos decimales

--- fields_megatk/models/sale_order.py
+++ fields_megatk/models/sale_order.py
@@ -45,6 +45,12 @@ class SaleorderLine(models.Model):
     obj_padre = fields.Many2one(related="order_id.user_id", string="ResponsableTem")
     x_series = fields.Text("Series")
     tax_editable = fields.Boolean('tax e.')
+    price_unit = fields.Float(
+        string="Unit Price",
+        compute='_compute_price_unit',
+        digits='Product Price',
+        store=True, readonly=False, required=True, precompute=True, digits=(16, 2))
+    
 
     def _prepare_invoice_line(self, **optional_values):
         invoice_item_sequence = 0
