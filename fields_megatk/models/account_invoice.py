d2d7d1bdef77ffb85df0cff1dac04738ef78d9ca
Alejandro Zelaya <jandro.zg123@gmail.com>
2024-12-11 14:53:48 -0600

    Cambiar comercial cuando el precio sea mayorista y pago en TC

--- fields_megatk/models/account_invoice.py
+++ fields_megatk/models/account_invoice.py
@@ -57,6 +57,106 @@ class Account_Move(models.Model):
                             raise UserError(_("ERROR: Contacto no tiene número de teléfono o móvil, agregar alguno de los dos antes de crear facturas al credito."))
         return super().create(vals_list)
         
+    @api.depends('amount_residual', 'move_type', 'state', 'company_id')
+    def _compute_payment_state(self):
+        stored_ids = tuple(self.ids)
+        if stored_ids:
+            self.env['account.partial.reconcile'].flush_model()
+            self.env['account.payment'].flush_model(['is_matched'])
+
+            queries = []
+            for source_field, counterpart_field in (('debit', 'credit'), ('credit', 'debit')):
+                queries.append(f'''
+                    SELECT
+                        source_line.id AS source_line_id,
+                        source_line.move_id AS source_move_id,
+                        account.account_type AS source_line_account_type,
+                        ARRAY_AGG(counterpart_move.move_type) AS counterpart_move_types,
+                        COALESCE(BOOL_AND(COALESCE(pay.is_matched, FALSE))
+                            FILTER (WHERE counterpart_move.payment_id IS NOT NULL), TRUE) AS all_payments_matched,
+                        BOOL_OR(COALESCE(BOOL(pay.id), FALSE)) as has_payment,
+                        BOOL_OR(COALESCE(BOOL(counterpart_move.statement_line_id), FALSE)) as has_st_line
+                    FROM account_partial_reconcile part
+                    JOIN account_move_line source_line ON source_line.id = part.{source_field}_move_id
+                    JOIN account_account account ON account.id = source_line.account_id
+                    JOIN account_move_line counterpart_line ON counterpart_line.id = part.{counterpart_field}_move_id
+                    JOIN account_move counterpart_move ON counterpart_move.id = counterpart_line.move_id
+                    LEFT JOIN account_payment pay ON pay.id = counterpart_move.payment_id
+                    WHERE source_line.move_id IN %s AND counterpart_line.move_id != source_line.move_id
+                    GROUP BY source_line.id, source_line.move_id, account.account_type
+                ''')
+
+            self._cr.execute(' UNION ALL '.join(queries), [stored_ids, stored_ids])
+
+            payment_data = defaultdict(lambda: [])
+            for row in self._cr.dictfetchall():
+                payment_data[row['source_move_id']].append(row)
+        else:
+            payment_data = {}
+
+        for invoice in self:
+            if invoice.payment_state == 'invoicing_legacy':
+                continue
+
+            currencies = invoice._get_lines_onchange_currency().currency_id
+            currency = currencies if len(currencies) == 1 else invoice.company_id.currency_id
+            reconciliation_vals = payment_data.get(invoice.id, [])
+            payment_state_matters = invoice.is_invoice(True)
+
+            if payment_state_matters:
+                reconciliation_vals = [x for x in reconciliation_vals if x['source_line_account_type'] in ('asset_receivable', 'liability_payable')]
+
+            new_pmt_state = 'not_paid'
+            if invoice.state == 'posted':
+
+                if payment_state_matters:
+                    if currency.is_zero(invoice.amount_residual):
+                        if any(x['has_payment'] or x['has_st_line'] for x in reconciliation_vals):
+                            if all(x['all_payments_matched'] for x in reconciliation_vals):
+                                new_pmt_state = 'paid'
+                            else:
+                                new_pmt_state = invoice._get_invoice_in_payment_state()
+                        else:
+                            new_pmt_state = 'paid'
+
+                            reverse_move_types = set()
+                            for x in reconciliation_vals:
+                                for move_type in x['counterpart_move_types']:
+                                    reverse_move_types.add(move_type)
+
+                            in_reverse = (invoice.move_type in ('in_invoice', 'in_receipt')
+                                          and (reverse_move_types == {'in_refund'} or reverse_move_types == {'in_refund', 'entry'}))
+                            out_reverse = (invoice.move_type in ('out_invoice', 'out_receipt')
+                                           and (reverse_move_types == {'out_refund'} or reverse_move_types == {'out_refund', 'entry'}))
+                            misc_reverse = (invoice.move_type in ('entry', 'out_refund', 'in_refund')
+                                            and reverse_move_types == {'entry'})
+                            if in_reverse or out_reverse or misc_reverse:
+                                new_pmt_state = 'reversed'
+
+                    elif reconciliation_vals:
+                        new_pmt_state = 'partial'
+
+            invoice.payment_state = new_pmt_state
+
+            # --- Agregar lógica adicional cuando el estado sea 'paid' ---
+            if new_pmt_state == 'paid':
+                for move in self:
+                    if move.payment_reference:  # Si el campo payment_reference tiene un valor
+                        _logger.warning("Entra al if de payment_reference " + move.payment_reference)
+                        # Buscar el pago relacionado por su referencia
+                        time.sleep(1)
+                        payment = self.env['account.payment'].search([('ref', '=', move.payment_reference)], limit=1)
+                        _logger.warning(payment)
+                        if payment and payment.journal_id.id == 1030:  # Verifica si el diario del pago es el ID 1030
+                            _logger.warning("Entra al if del diario")
+                            for line in move.invoice_line_ids:
+                                _logger.warning(f"Precio ID: {line.precio_id.id}")
+                                if line.precio_id.name.id == 1:  # Verifica si el precio_id es igual a 1
+                                    _logger.warning("Condición cumplida, cambiando el comercial.")
+                                    # Cambia el comercial al ID 78 usando write
+                                    move.write({'invoice_user_id': 60})
+                                    break  # Sal del bucle después de aplicar el cambi
+                                
     #mostrar boton en factura de borrados
     def go_draft(self):
         self.write({
