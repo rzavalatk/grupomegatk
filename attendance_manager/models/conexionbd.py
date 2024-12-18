6fa0adc2333f46d38e2ac701b806fa017e375822
Alejandro Zelaya <jandro.zg123@gmail.com>
2024-12-18 09:32:15 -0600

    Nuevos datos para usuario de la base

--- attendance_manager/models/conexionbd.py
+++ attendance_manager/models/conexionbd.py
@@ -13,7 +13,7 @@ class ConexionSQLServer(models.Model):
 
     resultado = fields.Text("Resultado de la consulta")
 
-    @api.model
+    #@api.model
     def obtener_datos_desde_sql(self):
         try:
             _loggin.warning("0")
