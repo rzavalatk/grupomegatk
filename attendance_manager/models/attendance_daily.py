762b6c9266ec7e26d418e040d343fc4b7075205c
Alejandro Zelaya <jandro.zg123@gmail.com>
2024-12-23 08:06:25 -0600

    Mod asis

--- attendance_manager/models/attendance_daily.py
+++ attendance_manager/models/attendance_daily.py
@@ -62,7 +62,7 @@ class AttendanceDaily(models.Model):
                 
                 for permiso in permisos:
                     logging.warning(permiso.employe_id.name)
-                    logging.warning(permiso.employe_id.fecha_inicio)
+                    logging.warning(permiso.fecha_inicio)
                 #Esto es para saber si es entrada o salida
                 marcacion_temp = hora_max_entrada
                 if not marcaciones:
