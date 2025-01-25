8b79733eb5457949e1bc3743c6b118d9824485bd
JiovannyM <jmorales.megatk@gmail.com>
2025-01-25 11:50:08 -0600

    base de automatizacion de blogs

---
+++ automatic_blog_post/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Módulo de automatizar publicaciones de blogs",
+    'summary': """
+        Módulo de automatizar publicaciones de blogs
+        """,
+    'description': """
+        publicar blogs en facebook e instagram
+    """,
+    'author': "Jiovanny Francisco Morales Hernandez",
+    "license": "LGPL-3",
+    'version': '0.1',
+    'depends': ['base'],
+    'data': [
+        "views/automatic_post_facebook.xml"
+    ],
+    'demo': [
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
