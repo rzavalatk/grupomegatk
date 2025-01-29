813fe2cc3da1122d6ada68ec642678829a2bec22
Alexander Reyes <azelaya@megatk.com>
2025-01-29 08:46:52 +0800

    [AR]Agregado archivo de security modulo estate

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,33 +1,15 @@
-# -*- coding: utf-8 -*-
 {
     'name': "Real Estate",
-
     'summary': """Modulo estate de prueba""",
-
-    'description': """
-        Modulo estate de prueba...
-    """,
-
+    'description': """Modulo estate de prueba...""",
     'author': "Alexander Reyes",
-    'website': "http://megaktk.net",
     "license": "LGPL-3",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
-    # for the full list
     'category': 'Uncategorized',
     'version': '0.1',
-
-    # any module necessary for this one to work correctly
     'depends': ['base'],
-
     'data': [
-        #'views/res_company.xml',
+        'security/ir.model.access.csv',
     ],
-
-    # always loaded
-   
-    # only loaded in demonstration mode
     'installable': True,
     'application': True,
     'auto_install': False,
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -4,4 +4,11 @@ class estate_property(models.Model):
     _name = "estate.property"
     _description = "description of properties"
     
-    
+    name = fields.Char(string="Nombre", required=True)
+    description = fields.Text(string="Descripcion")
+    price = fields.Float(string="Precio", required=True)
+    sold = fields.Boolean(string="Vendido")
+    image = fields.Binary(string="Imagen")
+    bedrooms = fields.Integer(string="Habitaciones", default=1)
+    bathrooms = fields.Integer(string="Banios", default=1)
+    living_area = fields.Integer(string="Area de vivienda", default=1)
