78c95222b54dd0063401196bdf191137ce228c1c
Alexander Reyes <azelaya@megatk.com>
2025-01-29 10:11:35 +0800

    [AR]Agregado archivo de security modulo estate

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -9,6 +9,7 @@
     'depends': ['base'],
     'data': [
         'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
     ],
     'installable': True,
     'application': True,
