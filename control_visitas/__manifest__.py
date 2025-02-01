f1261e86da616dbdc36437e1e96904af79e3151f
Alexander Reyes <azelaya@megatk.com>
2025-02-01 08:49:48 +0800

    [AR]Creando modulo de control de visitas

---
+++ control_visitas/__manifest__.py
@@ -0,0 +1,24 @@
+{
+    {
+        'name': 'Control Visitas',
+        'version': '1.0',
+        'description': 'Lleva el registro de las visitas a las diferentes areas de las sucursales , tanto en la de Tegucigalpa como en la SPS.',
+        'summary': 'Lleva el registro de las visitas a las diferenres areas de las sucursales.',
+        'author': 'Alexander Reyes',
+        'license': 'LGPL-3',
+        'category': 'uncategorized',
+        'depends': [
+            'base',
+            'mail',
+        ],
+        'data': [
+            ''
+        ],
+        'demo': [
+            ''
+        ],
+        'auto_install': True,
+        'application': True,
+        'auto?_install': False,
+    }
+}
---
+++ control_visitas/models/visitas.py
@@ -0,0 +1,12 @@
+from odoo import models, fields, api
+
+class Visitas(models.Model):
+    _name = 'registro.visitas'
+    _description = 'Lleva el registro de las visitas a las diferentes areas de las sucursales.'
+    
+    name = fields.Char(string = 'Nombre')
+    fecha = fields.Datetime(string = 'Fecha', default = fields.Datetime.now())
+    region = fields.Char(string = 'Region')
+    user_id = fields.Many2one('res.users', string = 'Usuario', default = lambda self: self.env.user)
+    
+    
