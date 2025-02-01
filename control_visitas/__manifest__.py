3220e15c9607c7708501b09c88262ab09af73e8e
Alexander Reyes <azelaya@megatk.com>
2025-02-01 11:48:14 +0800

    [AR]Creando modulo control visitas

---
+++ control_visitas/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    'name': 'Control de Visitas',
+    'version': '1.0',
+    'description': 'Controla el registro de visitas',
+    'summary': 'Controla el registro de visitas',
+    'author': 'Alexander Reyes',
+    'license': 'LGPL-3',
+    'category': 'uncategorized',
+    'depends': [
+        'base'
+    ],
+    'data': [
+        ''
+    ],
+    'demo': [
+        ''
+    ],
+    'auto_install': False,
+    'application': True,
+    'installable': True
+}
