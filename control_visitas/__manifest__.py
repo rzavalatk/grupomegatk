4cb54e2d6aa2fd2e0347b778cc78b4f5a6d55fab
Alexander Reyes <azelaya@megatk.com>
2025-02-03 07:36:08 +0800

    [AR]Agregando manifest e init

---
+++ control_visitas/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Control de Visitas',
+    'version': '1.0',
+    'description': 'Gestión de visitas en las sucursales',
+    'summary': 'Gestión de visitas',
+    'author': 'Alexander Reyes',
+    'license': 'LGPL-3',
+    'category': 'uncategorized',
+    'depends': [
+        'base'
+    ],
+    'data': [
+        ''
+    ],
+    'auto_install': False,
+    'application': True,
+}
