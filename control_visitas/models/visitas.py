a9b408cdc82d78fc3a5464d25c92f9db65a122a2
Alexander Reyes <azelaya@megatk.com>
2025-02-04 07:26:16 +0800

    [AR]Agregando metodos en el modelo

--- control_visitas/models/visitas.py
+++ control_visitas/models/visitas.py
@@ -7,7 +7,7 @@ class Visitas(models.Model):
     _description = 'Control de Visitas'
     
     name = fields.Char(string='Nombre')
-    fecha = fields.Date(string='Fecha', default=lambda self:self.datetime.now())
+    fecha = fields.Date(string='Fecha', default=datetime.now())
     region = fields.Char(string='Region', compute='_compute_region', store=True)
     user_id = fields.Many2one('res.users', string='Usuario', default=lambda self: self.env.user)
 
