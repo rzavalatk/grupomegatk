0593be1e8b4f4ffc1c721639629687ae1186c425
Alexander Reyes <azelaya@megatk.com>
2025-02-03 13:12:45 +0800

    [AR]Agregando modelo vistas menu

--- control_visitas/models/visitas.py
+++ control_visitas/models/visitas.py
@@ -1,4 +1,5 @@
 from odoo import models, fields, api
+from odoo.exceptions import ValidationError
 
 class Visitas(models.Model):
     _name = 'control.visitas'
@@ -6,5 +7,19 @@ class Visitas(models.Model):
     
     name = fields.Char(string='Nombre', required=True)
     fecha = fields.Date(string='Fecha')
-    region = fields.Char(string='Region')
+    region = fields.Char(string='Region', compute='_compute_region', store=True)
+    user_id = fields.Many2one('res.users', string='Usuario', default=lambda self: self.env.user)
+    
+    @api.depends('region')
+    def _compute_region(self):
+        for record in self:
+            record.region = record.user_id.region
+            
+    @api.model
+    def create(self, vals):
+        user = self.env.user
+        if user.region == "SPS" and vals.get('name') in ["Visita Lenka", "Visita Clínica", "Visita Administración"]:
+            raise ValidationError("No se pueden registrar esta visita en SPS")
+        return super(Visitas, self).create(vals)
+        
         
