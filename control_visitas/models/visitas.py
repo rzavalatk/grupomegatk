7dec1915826c66449c052e3b2411d520d1e1379e
Alexander Reyes <azelaya@megatk.com>
2025-02-04 14:16:53 -0600

    [AR]Agregando metodos en el modelo

--- control_visitas/models/visitas.py
+++ control_visitas/models/visitas.py
@@ -29,16 +29,11 @@ class Visitas(models.Model):
             record.fecha = datetime.now()
             
     @api.model
-    def create(self, vals):
+    def validarRegion(self, vals):
         user = self.env.user
-        #if self.fecha:
-        #    user_tz = pytz.timezone(self.env.context.get('tz') or user.tz)
-         #3   fecha = pytz.utc.localize(self.fecha).astimezone(user_tz)
-           # vals['fecha'] = fecha.replace(tzinfo=None)
-
         if user.ubicacion_vendedor == "SPS" and vals.get('name') in ["Visita Lenka", "Visita Clínica", "Visita Administración"]:
             raise ValidationError("No se pueden registrar esta visita en SPS")
-        return super(Visitas, self).create(vals)
+        return True
         
     def visita_administracion(self):
         self.create({'name': 'Visita Administración'})
