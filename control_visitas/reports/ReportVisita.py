cdfc685207bc4f28009c1be9ce088eed61053287
Alexander Reyes <azelaya@megatk.com>
2025-02-07 10:45:01 -0600

    [AR]Agregando metodos en el modelo

--- control_visitas/reports/ReportVisita.py
+++ control_visitas/reports/ReportVisita.py
@@ -1,11 +1,18 @@
 from odoo import api, models
+import logging
+
+_logger = logging.getLogger(__name__)
 
 class ReportVisita(models.AbstractModel):
     _name = 'report.control_visitas.report_visita'
 
     @api.model
     def _get_report_values(self, docids, data=None):
+        _logger.info(f"docids recibido: {docids}")
+        
         docs = self.env['control.visitas'].browse(docids)
+        _logger.info(f"Registros obtenidos: {docs}")
+        
         return {
             'docs': docs,
         }
