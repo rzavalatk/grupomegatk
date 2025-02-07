de4a1299413045c96b60ad86183830f335d36d1f
Alexander Reyes <azelaya@megatk.com>
2025-02-07 15:21:55 -0600

    [AR]Agregando metodos en el modelo

--- control_visitas/report/ReportVisita.py
+++ control_visitas/report/ReportVisita.py
@@ -4,20 +4,16 @@ import logging
 _logger = logging.getLogger(__name__)
 
 class ReportVisita(models.AbstractModel):
-    _name = 'control_visitas.report_pdf'
+    _name = 'control_visitas.report'
     _description = 'Reporte de Visitas' 
 
     @api.model
     def _get_report_values(self, docids, data=None):
         _logger.info(f"docids recibido: {docids}")
-        
+
         docs = self.env['control.visitas'].browse(docids)
         _logger.info(f"Registros obtenidos: {docs}")
 
-        report = self.env.ref('control_visitas.control_visitas.report_pdf')
-        report_values = report._get_report_values(self.env['control.visita'].browse(1))
-        
-        _logger.warning(f"Reporte obtenido: {report_values}")       
         return {
             'docs': docs,
         }
