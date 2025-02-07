9f689681c245feec1e1ab26dbfe5261eeb0618ca
Alexander Reyes <azelaya@megatk.com>
2025-02-07 07:31:36 -0600

    [AR]Agregando metodos en el modelo

--- control_visitas/models/visitas.py
+++ control_visitas/models/visitas.py
@@ -24,8 +24,8 @@ class Visitas(models.Model):
  
     @api.depends('user_id')
     def _compute_region_env(self):
-        if self.region_user == 3:
-            self.env.user.ubicacion_vendedor = 'TGU'
+        if self.env.user.ubicacion_vendedor == 3:
+            self.region_user = 'TGU'
             
     @api.model
     def create(self, vals):
