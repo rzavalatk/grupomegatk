4ff25fbccf9503df43a60c6fb29a84e5345268d1
Alexander Reyes <azelaya@megatk.com>
2025-02-11 11:55:22 -0600

    [AR]Agregando metodos en el modelo

--- control_visitas/models/visitas.py
+++ control_visitas/models/visitas.py
@@ -28,8 +28,8 @@ class Visitas(models.Model):
     def _compute_fecha(self):
         
         lista = self.env['control.visitas'].search([('name', '=', 'Visita Administración')])
-        rec = self.env['control.visitas'].browse([370])
-        _logger.warning(f"Registros obtenidos: {lista}")
+        rec = self.env['control.visitas'].browse(370)
+        _logger.warning(f"Registros obtenidos: {rec}")
         for record in self:
             record.fecha = datetime.now()
  
