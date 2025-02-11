30cac917afc774f2e749d8e77468f852528dc63c
Alexander Reyes <azelaya@megatk.com>
2025-02-11 07:33:09 -0600

    [AR]Agregando metodos en el modelo

--- control_visitas/models/visitas.py
+++ control_visitas/models/visitas.py
@@ -56,6 +56,20 @@ class Visitas(models.Model):
     def visita_clinica(self, vals):
         self.env['control.visitas'].create({'name': 'Visita Clínica'})
         
+    @api.model
+    def _get_report_values(self, docids, data=None):
+        _logger.warning(f"docids recibido: {docids}")
+
+        docs = self.env['control.visitas'].browse(docids)
+        _logger.warning(f"Registros obtenidos: {docs}")
+
+        # Llamar a la función para enviar el correo
+        self.enviar_correo_con_reporte(docs)
+
+        return {
+            'docs': docs,
+        }
+        
         
     def generar_pdf(self):
         report = self.env['ir.actions.report']._get_report_from_name('control_visitas.report_pdf') 
