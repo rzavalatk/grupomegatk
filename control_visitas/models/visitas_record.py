b9cbcbc134f4f44ae7fb61bd2e084e896576e0fc
Alexander Reyes <azelaya@megatk.com>
2025-02-14 15:23:08 -0600

    [AR]Agregando metodos en el modelo

--- control_visitas/models/visitas_record.py
+++ control_visitas/models/visitas_record.py
@@ -43,7 +43,7 @@ class Visitas_Record(models.Model):
             raise UserError("No hay registros de visitas en esa fecha")
         else:
             self.visita_diaria = visitas
-            _logger.warning(f"Registros encontrados: {self.visita_diaria}")
+            _logger.warning(f"Registros encontrados: {visitas}")
         
         template = self.env.ref(
             'control_visitas.email_template_registro_visitas')
