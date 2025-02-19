04e1fb030b8db0b15a737128148febfc0d9259cd
Jiovanny <jmorales.megatk@gmail.com>
2025-02-19 15:45:42 -0600

    [JM] atributos de empleados para vending machine

--- vm_odoo_conection/models/attributes_employee.py
+++ vm_odoo_conection/models/attributes_employee.py
@@ -3,6 +3,8 @@ from odoo import models, fields
 class attributes_employee(models.Model):
     _name = "attr.employee"
     _description = "Empleado"
+    _inherit = ['hr.employee']
+    
 
     credit = fields.Float(string ='Credito Semanal', default=100)
     number_card = fields.Integer(string='Número de tarjeta')
