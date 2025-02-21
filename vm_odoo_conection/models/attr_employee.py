a7f4744abc65682d617e2a3e695bd9b1a1992998
Jiovanny <jmorales.megatk@gmail.com>
2025-02-21 11:10:10 -0600

    [JM] atributos de empleados para vending machine

--- vm_odoo_conection/models/attr_employee.py
+++ vm_odoo_conection/models/attr_employee.py
@@ -4,5 +4,5 @@ class HrEmployee(models.Model):
     _inherit = 'hr.employee'
 
     credito = fields.Float(string='Crédito por semana', help='Crédito total del empleado', default= 100.0)
-    credito_disponible = fields.Float(string='Crédito Disponible', help='Crédito disponible del empleado', default = HrEmployee.credito)
+    credito_disponible = fields.Float(string='Crédito Disponible', help='Crédito disponible del empleado', default = credito)
     numero_tarjeta = fields.Char(string='Número de Tarjeta', help='Número de tarjeta del empleado', default='vacio')
