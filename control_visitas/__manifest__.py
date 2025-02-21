f789480e4c2e90d5a97ad840ca5e664d02904297
Alexander Reyes <azelaya@megatk.com>
2025-02-21 10:22:27 -0600

    [AR]Agregando generacion de reporte en excel

--- control_visitas/__manifest__.py
+++ control_visitas/__manifest__.py
@@ -13,12 +13,12 @@
     'data': [
         'security/groups.xml',
         'security/ir.model.access.csv',
-        'views/main_menu.xml',
         'views/control_visita_mail.xml',
         'views/registros_visitas_view.xml',
         'views/reportes_visitas_view.xml',
         'views/visitas_record_view.xml',
         'views/visitas_view.xml',
+        'views/main_menu.xml',
         'views/email_template.xml',
         'reports/reports.xml',   
         'reports/visitas_report.xml',   
