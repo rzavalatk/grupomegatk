fdde5241bae6c325cb1b495bda3f9ddceffa5c6a
Jiovanny <jmorales.megatk@gmail.com>
2025-02-26 09:42:32 -0600

    [JM] conexion de app con odoo

---
+++ vm_odoo_conection/controller/main.py
@@ -0,0 +1,23 @@
+from odoo import http
+from odoo.http import request
+
+class EmployeeController(http.Controller):
+
+    @http.route('/api/validate_card', type='json', auth='public', methods=['POST'])
+    def validate_card(self, **kwargs):
+        card_code = kwargs.get('card_code')
+        if not card_code:
+            return {'error': 'Código de tarjeta no proporcionado'}
+
+        # Buscar el empleado por el número de tarjeta
+        employee = request.env['hr.employee'].sudo().search([('numero_de_tarjeta', '=', card_code)], limit=1)
+        if not employee:
+            return {'error': 'Empleado no encontrado'}
+
+        # Devolver la información del empleado
+        return {
+            'name': employee.name,
+            'credito': employee.credito,
+            'credito_disponible': employee.credito_disponible,
+            'numero_de_tarjeta': employee.numero_de_tarjeta,
+        }
