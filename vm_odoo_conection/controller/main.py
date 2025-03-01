07a9854091e8c772ffc4a2a2f53d62a2c4d14dda
Jiovanny <jmorales.megatk@gmail.com>
2025-03-01 10:53:54 -0600

    [JM] conexion de app con odoo

--- vm_odoo_conection/controller/main.py
+++ vm_odoo_conection/controller/main.py
@@ -1,5 +1,6 @@
 from odoo import http
 from odoo.http import request
+import json
 import logging
 
 _logger = logging.getLogger(__name__)
@@ -8,24 +9,31 @@ class EmployeeController(http.Controller):
 
     @http.route('/api/validate_card', type='json', auth='public', methods=['POST'])
     def validate_card(self, **kwargs):
-        data = request.jsonrequest  # Acceder al cuerpo de la solicitud
-        _logger.info("Datos recibidos: %s", data)  # Log para ver los datos recibidos
-        card_code = data.get('card_code')
-        if not card_code:
-            _logger.error("Código de tarjeta no proporcionado")
-            return {'error': 'Código de tarjeta no proporcionado'}
+        try:
+            # Obtener el cuerpo de la solicitud JSON
+            data = json.loads(request.httprequest.data)
+            _logger.info("Datos recibidos: %s", data)  # Log para ver los datos recibidos
 
-        # Buscar el empleado por el número de tarjeta
-        employee = request.env['hr.employee'].sudo().search([('numero_tarjeta', '=', card_code)], limit=1)
-        if not employee:
-            _logger.error("Empleado no encontrado para el código de tarjeta: %s", card_code)
-            return {'error': 'Empleado no encontrado'}
+            # Extraer el código de la tarjeta
+            card_code = data.get('card_code')
+            if not card_code:
+                _logger.error("Código de tarjeta no proporcionado")
+                return {'error': 'Código de tarjeta no proporcionado'}
 
-        # Devolver la información del empleado
-        _logger.info("Empleado encontrado: %s", employee.name)
-        return {
-            'name': employee.name,
-            'credito': employee.credito,
-            'credito_disponible': employee.credito_disponible,
-            'numero_tarjeta': employee.numero_tarjeta,
-        }
+            # Buscar el empleado por el número de tarjeta
+            employee = request.env['hr.employee'].sudo().search([('numero_tarjeta', '=', card_code)], limit=1)
+            if not employee:
+                _logger.error("Empleado no encontrado para el código de tarjeta: %s", card_code)
+                return {'error': 'Empleado no encontrado'}
+
+            # Devolver la información del empleado
+            _logger.info("Empleado encontrado: %s", employee.name)
+            return {
+                'name': employee.name,
+                'credito': employee.credito,
+                'credito_disponible': employee.credito_disponible,
+                'numero_tarjeta': employee.numero_tarjeta,
+            }
+        except Exception as e:
+            _logger.error("Error en el controlador: %s", str(e))
+            return {'error': str(e)}
