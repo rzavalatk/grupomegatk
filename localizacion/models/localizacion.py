0ed7f8f12b2f7300617cd80ec93f54454e3db348
Alexander Reyes <azelaya@megatk.com>
2025-03-03 13:23:25 -0600

    [AR]Pruebas Modulo Localizacion

---
+++ localizacion/models/localizacion.py
@@ -0,0 +1,9 @@
+from odoo import models, fields, api
+from odoo.exceptions import ValidationError, UserError
+from datetime import date, datetime
+
+class Localizacion(models.Model):
+    _name = "localizacion"
+    _description = "Localizacion"
+    
+    pass
