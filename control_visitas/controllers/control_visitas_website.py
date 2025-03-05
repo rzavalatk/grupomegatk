110a1578e95544d283f94ca3bf3c714f01674f84
Alexander Reyes <azelaya@megatk.com>
2025-03-05 10:00:05 -0600

    [AR]Cambios modulo de visita

--- control_visitas/controllers/control_visitas_website.py
+++ control_visitas/controllers/control_visitas_website.py
@@ -10,7 +10,7 @@ class ControlVisitasWebsite(http.Controller):
     
     @http.route('/control_visitas', type='json', auth='public', website=True)
     def control_visitas_dashboard(self):
-        cod_reg = request.env["control.visitas"].user_id.ubicacion_vendedor
+        cod_reg = request.env["control.visitas"]
         reg = ""
         if cod_reg == "2":
             reg = "SPS"
