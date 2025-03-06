9c647ed6fb71326891f563ed31d4fbaae3a8447f
Jiovanny <jmorales.megatk@gmail.com>
2025-03-06 14:58:20 -0600

    [JM] conexion de app con odoo

--- vm_odoo_conection/controller/main.py
+++ vm_odoo_conection/controller/main.py
@@ -18,26 +18,53 @@ class EmployeeController(http.Controller):
             cardCode = data.get('cardCode')
             if not cardCode:
                 _logger.error("Código de tarjeta no proporcionado")
-                return {'error': 'Código de tarjeta no proporcionado'}
+                return {
+                    "jsonrpc": "2.0",
+                    "id": None,
+                    "result": {
+                        "status": "error",
+                        "status_msg": "Código de tarjeta no proporcionado"
+                    }
+                }
 
             # Buscar el empleado por el número de tarjeta
             employee = request.env['hr.employee'].sudo().search([('numero_tarjeta', '=', cardCode)], limit=1)
             if not employee:
                 _logger.error("Empleado no encontrado para el código de tarjeta: %s", cardCode)
-                return {'error': 'Empleado no encontrado'}
+                return {
+                    "jsonrpc": "2.0",
+                    "id": None,
+                    "result": {
+                        "status": "error",
+                        "status_msg": "Empleado no encontrado"
+                    }
+                }
 
-            # Devolver la información del empleado
-            _logger.info("Empleado encontrado: %s", employee.name)
+            # Devolver la información del empleado en formato JSON-RPC2
             response_data = {
-                'name': employee.name,
-                'credito': float(employee.credito),
-                'credito_disponible': float(employee.credito_disponible)
+                "jsonrpc": "2.0",
+                "id": None,
+                "result": {
+                    "status": "success",
+                    "status_msg": "Success",
+                    "data": {
+                        "name": employee.name,
+                        "credito": float(employee.credito),
+                        "credito_disponible": float(employee.credito_disponible)
+                    }
+                }
             }
 
-            # Log de la respuesta que se enviará
-            _logger.info("Respuesta JSON que se enviará: %s", response_data)
+            _logger.info("Respuesta JSON enviada: %s", response_data)
+            return response_data
 
-            return response_data  # Devolver el diccionario directamente
         except Exception as e:
             _logger.error("Error en el controlador: %s", str(e))
-            return {'error': str(e)}
+            return {
+                "jsonrpc": "2.0",
+                "id": None,
+                "result": {
+                    "status": "error",
+                    "status_msg": str(e)
+                }
+            }
