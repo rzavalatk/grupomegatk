a0515231582ae8111e3c4e49138207226b5b3e8f
Alejandro Zelaya <jandro.zg123@gmail.com>
2025-03-10 15:11:57 -0600

    Prubea de permisos [conexion

--- hr_payroll_community-16.0.1.0.5/hr_payroll_community/models/hr_payslip.py
+++ hr_payroll_community-16.0.1.0.5/hr_payroll_community/models/hr_payslip.py
@@ -276,9 +276,7 @@ class HrPayslip(models.Model):
             multi_leaves = []
             for day, hours, leave in day_leave_intervals:
                 logging.warning("-*-")
-                logging.warning(day)
-                logging.warning(hours)
-                logging.warning(leave)
+                logging.warning(leave[0])
                 work_hours = calendar.get_work_hours_count(
                     tz.localize(datetime.combine(day, time.min)),
                     tz.localize(datetime.combine(day, time.max)),
