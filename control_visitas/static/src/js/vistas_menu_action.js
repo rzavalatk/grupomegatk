e552e55d7566c2a54689d6fef6122d45904d4efb
Alexander Reyes <azelaya@megatk.com>
2025-03-10 15:26:39 -0600

    [AR]Cambios modulo de visita

--- control_visitas/static/src/js/vistas_menu_action.js
+++ control_visitas/static/src/js/vistas_menu_action.js
@@ -26,7 +26,7 @@ odoo.define('control_visitas.visitas_menu_action', function (require) {
         _deleteAdminRecord: function (ev) {
             ev.preventDefault();
     
-            console.log(ev.currentTarget.attrbutes.value.value);
+            console.log(ev.currentTarget.attrbutes[0].value);
             
 
             this._deleteRecord();
