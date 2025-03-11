dbf98b6081ba6701d6833872bdc5f867d2355fd8
Jiovanny <jmorales.megatk@gmail.com>
2025-03-11 11:58:10 -0600

    [JM] conexion de app con odoo

--- vm_odoo_conection/models/attr_employee.py
+++ vm_odoo_conection/models/attr_employee.py
@@ -6,3 +6,4 @@ class HrEmployee(models.Model):
     credito= fields.Float(string='Crédito por semana', help='Crédito total del empleado', default=100.0)
     credito_disponible= fields.Float(string='Crédito Disponible', help='Crédito disponible del empleado', default =0.0)
     numero_tarjeta= fields.Char(string='Número de Tarjeta', help='Número de tarjeta del empleado', default='vacio')
+    credito_a_recargar = fields.Float(string='Prueba')
