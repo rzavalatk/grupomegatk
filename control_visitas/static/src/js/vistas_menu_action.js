951092b02ccacf0387ec69149d75a655724c2071
Alexander Reyes <azelaya@megatk.com>
2025-03-11 15:06:46 -0600

    [AR]Cambios modulo de visita

--- control_visitas/static/src/js/vistas_menu_action.js
+++ control_visitas/static/src/js/vistas_menu_action.js
@@ -89,6 +89,7 @@ odoo.define('control_visitas.visitas_menu_action', function (require) {
                     method: 'visita_administracion',
                     args: [zona, self.filtro_dias],
                 }).then(function (resultado) {
+                    console.log( self.$el.find("#admin_value").text(resultado[`admin${reg}`]));
                     self.$el.find("#admin_value").text(resultado[`admin${reg}`]);
                 }).catch(function (error) {
                     console.error(error);
