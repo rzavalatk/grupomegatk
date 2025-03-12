ff35eba3313d1a8509cb3577c9b6ad215b1ab0ae
Jiovanny <jmorales.megatk@gmail.com>
2025-03-12 14:38:09 -0600

    [JM] conexion de app con odoo

--- vm_odoo_conection/models/attr_employee.py
+++ vm_odoo_conection/models/attr_employee.py
@@ -7,3 +7,15 @@ class HrEmployee(models.Model):
     credito_disponible= fields.Float(string='Crédito Disponible', help='Crédito disponible del empleado', default =0.0)
     numero_tarjeta= fields.Char(string='Número de Tarjeta', help='Número de tarjeta del empleado', default='vacio')
     credito_a_recargar = fields.Float(string='Prueba')
+
+    def recargar_credito(self):
+        self.credito = self.credito_a_recargar
+        return True
+
+    @api.model
+    def actualizar_creditos_mensualmente(self):
+        hoy = date.today()
+        if hoy.day == 1:  # Verifica si es el primer día del mes
+            empleados = self.search([])  # Obtiene todos los empleados
+            for empleado in empleados:
+                empleado.credito = empleado.credito_a_recargar
