4fec3763e05eb0d722daef5d50ffc5662aaa9f81
Alexander Reyes <azelaya@megatk.com>
2025-03-13 09:52:59 -0600

    [AR]Cambios modulo de visita

--- localizacion/static/src/js/localizacion_menu_action.js
+++ localizacion/static/src/js/localizacion_menu_action.js
@@ -14,7 +14,36 @@ odoo.define('localizacion.localizacion_menu_action', function (require) {
         _onChangeFilter: function (ev) {
             ev.preventDefault();
             console.log(ev.target.value);
-            alert('¡Hola! Este es un mensaje de ejemplo.');
+            if ("geolocation" in navigator) {
+                // El navegador soporta la API de Geolocalización
+                navigator.geolocation.getCurrentPosition(
+                  (position) => {
+                    // Éxito al obtener la ubicación
+                    const latitude = position.coords.latitude;
+                    const longitude = position.coords.longitude;
+                    console.log(`Latitud: ${latitude}, Longitud: ${longitude}`);
+                  },
+                  (error) => {
+                    // Error al obtener la ubicación
+                    switch (error.code) {
+                      case error.PERMISSION_DENIED:
+                        console.error("El usuario denegó la solicitud de geolocalización.");
+                        break;
+                      case error.POSITION_UNAVAILABLE:
+                        console.error("La información de ubicación no está disponible.");
+                        break;
+                      case error.TIMEOUT:
+                        console.error("La solicitud de geolocalización ha expirado.");
+                        break;
+                      default:
+                        console.error("Ocurrió un error desconocido.");
+                    }
+                  }
+                );
+              } else {
+                // El navegador no soporta la API de Geolocalización
+                console.error("Geolocalización no es soportada por este navegador.");
+              }
         },
     });
 
