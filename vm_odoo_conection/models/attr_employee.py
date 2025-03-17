fec3201b4b7b4dc3dde08f3caa5e1d5a57309b4f
Jiovanny <jmorales.megatk@gmail.com>
2025-03-17 10:28:37 -0600

    [JM] conexion de app con odoo

--- vm_odoo_conection/models/attr_employee.py
+++ vm_odoo_conection/models/attr_employee.py
@@ -8,7 +8,12 @@ class HrEmployee(models.Model):
     numero_tarjeta= fields.Char(string='Número de Tarjeta', help='Número de tarjeta del empleado')
     gastado = fields.Float(string='Gastado', help='Credito gastado desde el 1 del mes hasta la fecha')
 
-    def reiniciar_gastado(self):
+    def reiniciar_credito_disponible(self):
         empleados = self.search([])  
         for empleado in empleados:
-            empleado.write({'gastado': empleado.credito})
+            empleado.write({'credito_disponible': empleado.credito})
+
+    def gastos(self):
+        empleados = self.search([])
+        for empleado in empleados:
+            empleado.write({'gastado': empleado.credito - empleado.credito_disponible})
