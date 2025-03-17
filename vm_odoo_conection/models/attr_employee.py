145a37b41e508291c7def9c93b69a38b24c85fa9
Jiovanny <jmorales.megatk@gmail.com>
2025-03-17 10:50:57 -0600

    [JM] conexion de app con odoo

--- vm_odoo_conection/models/attr_employee.py
+++ vm_odoo_conection/models/attr_employee.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 class HrEmployee(models.Model):
     _inherit = 'hr.employee'
@@ -6,14 +6,17 @@ class HrEmployee(models.Model):
     credito= fields.Float(string='Crédito asignado', help='Crédito total del empleado')
     credito_disponible= fields.Float(string='Crédito Disponible', help='Crédito disponible del empleado')
     numero_tarjeta= fields.Char(string='Número de Tarjeta', help='Número de tarjeta del empleado')
-    gastado = fields.Float(string='Gastado', help='Credito gastado desde el 1 del mes hasta la fecha')
+    gastado = fields.Float(string='Gastado',help='Crédito gastado desde el 1 del mes hasta la fecha',compute='_compute_gastado',store=True)
 
     def reiniciar_credito_disponible(self):
         empleados = self.search([])  
         for empleado in empleados:
             empleado.write({'credito_disponible': empleado.credito})
 
-    def gastos(self):
-        empleados = self.search([])
-        for empleado in empleados:
-            empleado.write({'gastado': empleado.credito - empleado.credito_disponible})
+    @api.depends('credito', 'credito_disponible')
+    def _compute_gastado(self):
+        """
+        Método para calcular el campo 'gastado' como 'credito - credito_disponible'.
+        """
+        for empleado in self:
+            empleado.gastado = empleado.credito - empleado.credito_disponible
