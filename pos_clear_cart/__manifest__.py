299e0f3f62c649ad4a7b27d9bded6f17b51723bb
Alexander Reyes <azelaya@megatk.com>
2025-03-18 09:40:46 -0600

    [AR]Implementaciones POS - boton de limpieza de carro de compra

--- pos_clear_cart/__manifest__.py
+++ pos_clear_cart/__manifest__.py
@@ -8,8 +8,8 @@
     "depends": ["base", "point_of_sale"],
     "data": [],
     "assets": {
-        "point_of_sale.assets": ["bsi_pos_clear_cart/static/src/js/pos_clear_cart.js",
-                                 "bsi_pos_clear_cart/static/src/xml/pos_clear_cart.xml",],
+        "point_of_sale.assets": ["pos_clear_cart/static/src/js/pos_clear_cart.js",
+                                 "pos_clear_cart/static/src/xml/pos_clear_cart.xml",],
         
     },
     "images": ["static/description/Pos Clear Cart Banner.gif"],
