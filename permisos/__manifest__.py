708e9e20635097020bbb426da9cd3aaaa6891c2f
Jiovanny <jmorales.megatk@gmail.com>
2025-04-07 07:43:40 -0600

    [JM] informacion a recibir de la vm

--- permisos/__manifest__.py
+++ permisos/__manifest__.py
@@ -27,12 +27,10 @@
         'security/ir.model.access.csv',
         "wizard/wizard_solicitar_permiso.xml",
         'views/default_views.xml',
-        'views/permisos_views.xml',
         'views/print_permission.xml',
+        'views/permisos_views.xml',
         'views/hr_views.xml',
         'views/email_template.xml',
-        
-        
-        
+        'static/src/css/report_styles.css'
     ]
 }
