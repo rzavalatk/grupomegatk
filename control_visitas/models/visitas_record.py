68a3cab1b964369b90b4c072bbf2fb382e3eb7b5
Alexander Reyes <azelaya@megatk.com>
2025-04-11 14:07:18 -0600

    [AR]Modificaciones al modulo de visitas

--- control_visitas/models/visitas_record.py
+++ control_visitas/models/visitas_record.py
@@ -53,9 +53,9 @@ class Visitas_Record(models.Model):
                 self.write({'state': 'borrador'})
                 raise UserError("La fecha final debe ser mayor a la fecha inicial")
             elif self.fecha_final == self.fecha_reporte:
-                visitas = self.env['control.visitas'].sudo().search([('fecha', '=', self.fecha_reporte),('region', 'in', ['TGU', 'SPS'])])
+                visitas = self.env['control.visitas'].search([('fecha', '=', self.fecha_reporte),'|',('region', '=','TGU'),('region','=','SPS')])
             else:
-                 visitas = self.env['control.visitas'].sudo().search([('fecha', '>=', self.fecha_reporte),('fecha', '<=', self.fecha_final),'|',('region', '=','TGU'),('region','=','SPS')])
+                 visitas = self.env['control.visitas'].search([('fecha', '>=', self.fecha_reporte),('fecha', '<=', self.fecha_final),'|',('region', '=','TGU'),('region','=','SPS')])
         
         for visita in visitas:
             _logger.warning(f"Visita ID desde Master {visita.id} - Región: {visita.region}")
