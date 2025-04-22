dd95ac870697befc8f311da1eee20368a201b8ab
Alejandro Zelaya <jandro.zg123@gmail.com>
2025-04-22 14:16:43 -0600

    Busqueda day_leave_intervals

--- ohrms_core-16.0.1.0.0/hr_payroll_community/models/hr_payslip.py
+++ ohrms_core-16.0.1.0.0/hr_payroll_community/models/hr_payslip.py
@@ -285,7 +285,7 @@ class HrPayslip(models.Model):
             multi_leaves = []
             for day, hours, leave in day_leave_intervals:
                 logging.warning("-*-")
-                logging.warning(leave[0])
+                logging.warning(leave[0].holiday_id)
                 work_hours = calendar.get_work_hours_count(
                     tz.localize(datetime.combine(day, time.min)),
                     tz.localize(datetime.combine(day, time.max)),
@@ -296,7 +296,7 @@ class HrPayslip(models.Model):
                         if each.id:
                             multi_leaves.append(each.holiday_id)
                 else:
-                    holiday = self.env['hr.holidays'].search([('id','=',leave[0].holiday_id)],limit=1)
+                    holiday = self.env['hr.leave'].search([('id','=',leave[0].holiday_id)],limit=1)
                     current_leave_struct = leaves.setdefault(
                         holiday.holiday_status_id, {
                             'name': holiday.holiday_status_id.name or _(
