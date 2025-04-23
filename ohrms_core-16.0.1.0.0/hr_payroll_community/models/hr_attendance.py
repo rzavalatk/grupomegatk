c3e9f13c68ea094b7561e24f1c8ec89c844d4314
Alejandro Zelaya <jandro.zg123@gmail.com>
2025-04-23 15:24:44 -0600

    Modificación de asistencias

--- ohrms_core-16.0.1.0.0/hr_payroll_community/models/hr_attendance.py
+++ ohrms_core-16.0.1.0.0/hr_payroll_community/models/hr_attendance.py
@@ -52,7 +52,7 @@ class AttendanceRuleInput(models.Model):
             out_date = check_out_utc6.date()
             # obtenemos el horario de trabajo del contrato
             working_hours = self.env['resource.calendar.attendance'].search([('id', '=', contract_id.resource_calendar_id.id)])
-            
+            _logger.warning("working_hours %s",working_hours)
             for hours in working_hours:
                 # obtenemos la información del horario de trabajo
                 start_time = hours.hour_from
