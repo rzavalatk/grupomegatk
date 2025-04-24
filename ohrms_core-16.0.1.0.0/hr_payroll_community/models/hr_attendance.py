5288ba61de535e5a6802fbdd0716a390def29f9e
Alejandro Zelaya <jandro.zg123@gmail.com>
2025-04-24 09:56:39 -0600

    Prueba de asistencias y horario laboral

--- ohrms_core-16.0.1.0.0/hr_payroll_community/models/hr_attendance.py
+++ ohrms_core-16.0.1.0.0/hr_payroll_community/models/hr_attendance.py
@@ -29,7 +29,7 @@ class AttendanceRuleInput(models.Model):
             
             _logger.warning("Horario: %s, %s, %s, %s", start_time, day_period, days_of_week, dia_permiso)
             
-            if (days_of_week == dia_permiso):
+            if (str(days_of_week) == str(dia_permiso)):
                 _logger.warning("SI entre al if 1")
                 if (day_period == 'morning'): 
                     #calcular deducciones
