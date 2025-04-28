2022ae51dff31e31416e8bfde2b94becbe97b5b1
Alexander Reyes <azelaya@megatk.com>
2025-04-28 10:58:41 -0600

    [AR]Prueba modulo localizacion

---
+++ gps_tracking/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'Rastreo GPS',
+    'version': '1.0',
+    'description': 'Modulo para rastreo GPS',
+    'summary': 'Modulo para rastreo GPS',
+    'author': 'Alexander Reyes',
+    'license': 'LGPL-3',
+    'category': 'uncategorized',
+    'depends': [
+        'base',
+    ],
+    'data': [
+        'views/gps_tracking_view.xml',
+    ],
+    
+    'auto_install': False,
+    'application': True,
+    'installable': True
+}
---
+++ gps_tracking/models/gps_device_location.py
@@ -0,0 +1,44 @@
+import datetime
+from odoo import models, fields, api
+import requests
+import logging
+
+logger = logging.getLogger(__name__)
+
+class GpsDeviceLocation(models.Model):
+    _name = 'gps.device.location'
+    _description = 'Ubicación del Dispositivo GPS'
+
+    device_id = fields.Char('ID del Dispositivo')
+    latitude = fields.Float('Latitud')
+    longitude = fields.Float('Longitud')
+    speed = fields.Float('Velocidad')
+    timestamp = fields.Datetime('Fecha y Hora')
+    address = fields.Char('Dirección')
+    
+    @api.model
+    def fetch_traccar_positions(self):
+        # Configura tu IP, usuario y contraseña de Traccar
+        traccar_url = 'http://18.222.109.183:8082/api/positions'
+        auth = ('areyes@megatk.com', 'admin')  # Cambiar por tus credenciales reales
+
+        try:
+            response = requests.get(traccar_url, auth=auth, timeout=10)
+            response.raise_for_status()
+        except requests.RequestException as e:
+            raise Exception(f"Error al conectar con Traccar: {e}")
+
+        positions = response.json()
+        
+        for pos in positions:
+            logger.info(f"Posicion: {pos}")
+
+        # for pos in positions:
+        #     self.create({
+        #         'device_id': pos.get('deviceId'),
+        #         'latitude': pos.get('latitude'),
+        #         'longitude': pos.get('longitude'),
+        #         'speed': pos.get('speed'),
+        #         'timestamp': datetime.utcfromtimestamp(pos.get('fixTime') // 1000) if pos.get('fixTime') else False,
+        #         'address': pos.get('address', ''),
+        #     })
