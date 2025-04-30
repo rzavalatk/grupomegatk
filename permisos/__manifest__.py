16f60fef31fb38ba220c44b25b296f8997c4e0bc
Alejandro Zelaya <jandro.zg123@gmail.com>
2025-04-30 15:26:10 -0600

    Se añade los campos de vacaciones

--- permisos/__manifest__.py
+++ permisos/__manifest__.py
@@ -12,16 +12,11 @@
     'website': "https://www.megatk.net",
     "license": "LGPL-3",
 
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
     'category': 'Human Resources',
     'version': '0.1',
 
-    # any module necessary for this one to work correctly
     'depends': ['base','hr'],
 
-    # always loaded
     'data': [
         "security/groups.xml",
         'security/ir.model.access.csv',
@@ -34,5 +29,11 @@
         'views/hr_leave_view.xml',
         'views/hr_leave_type_view.xml',
         
-    ]
+    ],
+    'assets': {
+        'web.assets_backend': [
+            'permisos/static/src/xml/templates_timeoff.xml',
+        ],
+    },
+
 }
