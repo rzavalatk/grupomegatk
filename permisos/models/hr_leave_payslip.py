09b40c19e461fa0352e5727f3e9d8c91c61002e2
Alejandro Zelaya <jandro.zg123@gmail.com>
2025-05-02 15:52:23 -0600

    Permisos

--- permisos/models/hr_leave_payslip.py
+++ permisos/models/hr_leave_payslip.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
-from datetime import datetime, timedelta, time
-from odoo import api, fields, models, tools, _
+from odoo import fields, models
+from datetime import datetime, time
 from pytz import timezone, utc
 
 import logging
