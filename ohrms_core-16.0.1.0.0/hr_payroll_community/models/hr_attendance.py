5456ca442cbdc1bda395f288e996159dd61a0861
Alejandro Zelaya <jandro.zg123@gmail.com>
2025-05-03 11:15:54 -0600

    Prueba de permisos

--- ohrms_core-16.0.1.0.0/hr_payroll_community/models/hr_attendance.py
+++ ohrms_core-16.0.1.0.0/hr_payroll_community/models/hr_attendance.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from datetime import datetime, timedelta, time
-from odoo import models
+from odoo import models, fields
 import logging
 import pytz
 
@@ -43,15 +43,50 @@ class AttendanceRuleInput(models.Model):
                         deduccion = costo_hora*4
         
         return deduccion
+            
     
     def get_inputs(self, contract_ids, date_from, date_to):
         """This Compute the other inputs to employee payslip.
                            """
         res = super(AttendanceRuleInput, self).get_inputs(contract_ids, date_from, date_to)
+        
+        
+        
         contract_obj = self.env['hr.contract']
         contract_id = contract_obj.browse(contract_ids[0].id)
         emp_id = contract_obj.browse(contract_ids[0].id).employee_id
         attendances = self.env['hr.attendance'].search([('employee_id', '=', emp_id.id),('check_in', '>=', date_from),('check_out', '<=', date_to)])
+        
+        day_from = datetime.combine(fields.Date.from_string(date_from),
+                                        time.min)
+        day_to = datetime.combine(fields.Date.from_string(date_to),
+                                    time.max)
+        
+        costo_dia = contract_id.wage/30
+        costo_hora = costo_dia/8
+        costo_minuto = costo_hora/60
+        
+        day_leave_intervals = []
+        
+        day_leave_intervals = contract_id.employee_id.list_leaves(day_from,
+                                                                day_to,
+                                                                calendar=contract_id.resource_calendar_id)
+        
+        multi_leaves = []
+        for day, hours, leave in day_leave_intervals:
+            
+            if len(leave) > 1:
+                for each in leave:
+                    if each.id:
+                        multi_leaves.append(each.holiday_id)
+            else:
+                if leave[0].holiday_id.holiday_status_id.deducciones:
+                    for result in res:
+                        if result.get('code') == 'DED_PRM':
+                            total = leave[0].holiday_id.dias * costo_dia + leave[0].holiday_id.horas * costo_hora + leave[0].holiday_id.minutos * costo_minuto
+                            result['amount'] += total
+                            
+        
         for attendance in attendances:
             check_in_utc6 = attendance.check_in.astimezone(honduras_tz)
             check_out_utc6 = attendance.check_out.astimezone(honduras_tz)
