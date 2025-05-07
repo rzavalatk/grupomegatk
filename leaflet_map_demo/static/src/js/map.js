2146217eb04f705d2ae641c2a5b9066319bf91fd
Alexander Reyes <azelaya@megatk.com>
2025-05-07 14:40:03 -0600

    [AR]Prueba modulo localizacion

--- leaflet_map_demo/static/src/js/map.js
+++ leaflet_map_demo/static/src/js/map.js
@@ -1,17 +1,34 @@
-odoo.define('leaflet_map_demo.map', function (require) {
-    'use strict';
-
-    if (!document.getElementById('mapid')) return;
-
-    document.addEventListener("DOMContentLoaded", function () {
-        var map = L.map('mapid').setView([19.4326, -99.1332], 13);  // CDMX
-
-        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            attribution: '© OpenStreetMap contributors'
-        }).addTo(map);
-
-        L.marker([19.4326, -99.1332]).addTo(map)
-            .bindPopup('Ubicación inicial.')
-            .openPopup();
+// static/src/js/mapa.js
+odoo.define('tu_modulo.LeafletMap', function (require) {
+    "use strict";
+    
+    var Widget = require('web.Widget');
+    
+    var LeafletMap = Widget.extend({
+        template: 'leaflet_map_demo.leaflet_map',
+        
+        start: function () {
+            this._super.apply(this, arguments);
+            
+            // Inicializar el mapa después de que el DOM esté listo
+            this._initMap();
+        },
+        
+        _initMap: function () {
+            // Crear el mapa
+            this.map = L.map(this.$el[0]).setView([51.505, -0.09], 13);
+            
+            // Añadir capa de tiles (OpenStreetMap por defecto)
+            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            }).addTo(this.map);
+            
+            // Añadir un marcador
+            L.marker([51.5, -0.09]).addTo(this.map)
+                .bindPopup('Ubicación de ejemplo')
+                .openPopup();
+        }
     });
+    
+    return LeafletMap;
 });
