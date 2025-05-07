34b9dd0a7a003bf640a01f34a495ea1069f9e509
Alexander Reyes <azelaya@megatk.com>
2025-05-07 09:05:42 -0600

    [AR]Prueba modulo localizacion

--- leaflet_map_demo/leaflet_map_demo/__manifest__.py
+++ leaflet_map_demo/leaflet_map_demo/__manifest__.py
@@ -6,5 +6,13 @@
         'views/assets.xml',
         'views/map_view.xml',
     ],
+    'assets': {
+        'web.assets_backend': [
+            '/leaflet_map_demo/static/src/js/map.js',
+            'https://unpkg.com/leaflet@1.9.3/dist/leaflet.js',
+            'https://unpkg.com/leaflet@1.9.3/dist/leaflet.css',
+        ],
+    },
     'installable': True,
+    'application': True
 }
