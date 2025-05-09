b038715e1f608ffd3e21c067efc6623627e125c4
Alexander Reyes <azelaya@megatk.com>
2025-05-09 08:40:45 -0600

    [AR]Prueba modulo localizacion

--- leaflet_map_form/__manifest__.py
+++ leaflet_map_form/__manifest__.py
@@ -3,6 +3,8 @@
     "version": "1.0",
     "depends": ["base", "web"],
     "data": [
+        "security/groups.xml"
+        "security/ir.model.access.csv",
         "views/leaflet_view.xml"
     ],
     "assets": {
