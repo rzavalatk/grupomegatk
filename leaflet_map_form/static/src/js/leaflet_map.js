1c5d654aa1977985883c89e12124a94941b3d9f2
Alexander Reyes <azelaya@megatk.com>
2025-05-09 08:48:12 -0600

    [AR]Prueba modulo localizacion

--- leaflet_map_form/static/src/js/leaflet_map.js
+++ leaflet_map_form/static/src/js/leaflet_map.js
@@ -6,10 +6,13 @@ odoo.define('leaflet_map_form.leaflet_map', function (require) {
     const LeafletFormController = FormController.include({
         _renderView: async function () {
             await this._super(...arguments);
+            console.log("1");
+            
             this._renderLeafletMap();
         },
 
         _renderLeafletMap: function () {
+            console.log("2");
             const record = this.model.get(this.handle);
             const lat = record.data.latitude;
             const lng = record.data.longitude;
