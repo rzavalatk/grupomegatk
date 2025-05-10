b06a727e059b3053b6cec9081fb852d88b64eb95
Alexander Reyes <azelaya@megatk.com>
2025-05-10 09:20:11 -0600

    [AR]Prueba modulo localizacion

--- gps_tracking/static/src/js/tracking_menu_action.js
+++ gps_tracking/static/src/js/tracking_menu_action.js
@@ -1,20 +1,15 @@
-odoo.define('gps_tracking.tracking_menu_action', function (require) {
-    "use strict";
+/** @odoo-module **/
 
-    core = require('web.core');
-    var AbstractAction = require('web.AbstractAction');
-    var rpc = require('web.rpc');
-    var ajax = require('web.ajax');
-    var CustomCardMenu = AbstractAction.extend({
-        template: 'TrackingCardMenu',
+import { registry } from "@web/core/registry";
+import { AbstractAction } from "@web/webclient/actions/abstract_action";
+import { loadJS } from "@web/core/assets";
+import { patch } from "@web/core/utils/patch";
 
-        start: function () {
-            console.log("funciona");
-            
-            return this._super.apply(this, arguments);
-        },
-    });
+export class TrackingMenuAction extends AbstractAction {
+    setup() {
+        super.setup();
+        console.log("funciona");
+    }
+}
 
-    core.action_registry.add('gps_tracking_tag', CustomCardMenu);
-    return CustomCardMenu;
-});
+registry.category("actions").add("gps_tracking_tag", TrackingMenuAction);
