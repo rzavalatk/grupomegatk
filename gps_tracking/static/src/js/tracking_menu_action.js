b9d8fe4d3314c984b6dd1c28a7dcaf51d7363245
Alexander Reyes <azelaya@megatk.com>
2025-05-10 10:58:17 -0600

    [AR]Prueba modulo localizacion

--- gps_tracking/static/src/js/tracking_menu_action.js
+++ gps_tracking/static/src/js/tracking_menu_action.js
@@ -8,23 +8,8 @@ odoo.define('gps_tracking.tracking_menu_action', function (require) {
         template: 'TrackingCardMenu',  // <-- Esto es importante
         start: function () {
             console.log("funciona");
-            this._initLeafletMap();
+
             return this._super.apply(this, arguments);
-        },
-
-        _initLeafletMap: function () {
-            // Espera a que el DOM esté cargado
-            setTimeout(() => {
-                const map = L.map('map').setView([14.0989839, -87.1899595], 13); // Ciudad de México
-
-                L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                    attribution: '&copy; OpenStreetMap contributors'
-                }).addTo(map);
-
-                L.marker([14.0989839, -87.1899595]).addTo(map)
-                    .bindPopup('Ubicación inicial')
-                    .openPopup();
-            }, 0);
         },
     });
 
