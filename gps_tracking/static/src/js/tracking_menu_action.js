3d6ab003ab361fd3c07f13c32ff6a7db540b3768
Alexander Reyes <azelaya@megatk.com>
2025-05-12 13:21:07 -0600

    [AR]Prueba modulo localizacion

--- gps_tracking/static/src/js/tracking_menu_action.js
+++ gps_tracking/static/src/js/tracking_menu_action.js
@@ -5,12 +5,23 @@ odoo.define('gps_tracking.tracking_menu_action', function (require) {
     const core = require('web.core');
 
     const CustomCardMenu = AbstractAction.extend({
-        template: 'TrackingCardMenu',  // <-- Esto es importante
-        start: function () {
-            console.log("funciona");
+        template: 'TrackingCardMenu',
 
+        events: {
+            "click .btn-success": "_onClickPrueba",
+            "click .btn-warning": "_onClickPrueba",
+        },
+
+        _onClickPrueba: function () {
+            console.log("hola");
+        },
+
+        start: function () {
+            
             return this._super.apply(this, arguments);
         },
+
+
     });
 
     core.action_registry.add('gps_tracking_tag', CustomCardMenu);  // Este es el tag que se llama en el XML
