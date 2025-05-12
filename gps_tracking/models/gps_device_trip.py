4ea32200db349f774eb9eafc8c032ffa0db88fd6
Alexander Reyes <azelaya@megatk.com>
2025-05-12 15:49:09 -0600

    [AR]Prueba modulo localizacion

--- gps_tracking/models/gps_device_trip.py
+++ gps_tracking/models/gps_device_trip.py
@@ -38,25 +38,25 @@ class GpsDeviceTrip(models.Model):
 
         return super(GpsDeviceTrip, self).create(vals)
     
-    def check_trips(self):
-        trip = self.search([('state','in',['ongoing','finished','new'])])
-        _logger.warning(f"Cantidad de viajes: {len(trip)}")
-        new  = 0
-        ongoing = 0
-        finished = 0
-        for trip in self:          
-            if trip.state == 'new':
-                new += 1
-            if trip.state == 'ongoing':
-                ongoing += 1
-            if trip.state == 'finshed':
-                finished += 1
+    # def check_trips(self):
+    #     trip = self.search([('state','in',['ongoing','finished','new'])])
+    #     _logger.warning(f"Cantidad de viajes: {len(trip)}")
+    #     new  = 0
+    #     ongoing = 0
+    #     finished = 0
+    #     for trip in self:          
+    #         if trip.state == 'new':
+    #             new += 1
+    #         if trip.state == 'ongoing':
+    #             ongoing += 1
+    #         if trip.state == 'finshed':
+    #             finished += 1
                 
-        if new > 0 or ongoing > 0:
-            return True
+    #     if new > 0 or ongoing > 0:
+    #         return True
                 
-        if finished == len(trip):
-            return False
+    #     if finished == len(trip):
+    #         return False
                 
     @api.model
     def start_trip(self, device_id):
@@ -69,38 +69,38 @@ class GpsDeviceTrip(models.Model):
             'name': f'Viaje {device_id} - {datetime.now().strftime("%Y-%m-%d %H:%M:%S")}',
             'device_id': device_id,
             'start_time': fields.Datetime.now(),
-            'state': 'new'
+            'state': 'ongoing'
         })
         
         self.fetch_device_positions()
         self.write({'state': 'ongoing'})
         return trip
     
-    def action_start_trip(self):
-        for trip in self:
-            existing = self.search([
-                ('device_id','=',trip.device_id),
-                ('state','=','ongoing'),
-                ('id','!=',trip.id),
-                ], limit=1)
+    # def action_start_trip(self):
+    #     for trip in self:
+    #         existing = self.search([
+    #             ('device_id','=',trip.device_id),
+    #             ('state','=','ongoing'),
+    #             ('id','!=',trip.id),
+    #             ], limit=1)
             
-            if existing:
-                raise ValidationError(f"Ya hay un viaje en curso para este dispositivo")
+    #         if existing:
+    #             raise ValidationError(f"Ya hay un viaje en curso para este dispositivo")
             
-            trip.write({
-                'name': f'Viaje {trip.device_id} - {datetime.now().strftime("%Y-%m-%d %H:%M:%S")}',
-                'start_time': fields.Datetime.now(),
-                'state': 'ongoing',
-            })
+    #         trip.write({
+    #             'name': f'Viaje {trip.device_id} - {datetime.now().strftime("%Y-%m-%d %H:%M:%S")}',
+    #             'start_time': fields.Datetime.now(),
+    #             'state': 'ongoing',
+    #         })
             
-        active = self.check_trips()
-        if active:
+    #     active = self.check_trips()
+    #     if active:
 
-            cron = self.env.ref('gps_tracking.ir_cron_update_gps_positions')
-            cron.write({'active': True, 'nextcall': fields.Datetime.now()})
-        else:
-            cron = self.env.ref('gps_tracking.ir_cron_update_gps_positions')
-            cron.write({'active': False, 'nextcall': fields.Datetime.now()})
+    #         cron = self.env.ref('gps_tracking.ir_cron_update_gps_positions')
+    #         cron.write({'active': True, 'nextcall': fields.Datetime.now()})
+    #     else:
+    #         cron = self.env.ref('gps_tracking.ir_cron_update_gps_positions')
+    #         cron.write({'active': False, 'nextcall': fields.Datetime.now()})
 
     def finish_trip(self):
         """Finalizar un viaje"""
@@ -180,27 +180,8 @@ class GpsDeviceTrip(models.Model):
     
     def cron_fetch_positions(self):
         viajes = self.search([('state','=','ongoing')])
-        
-
         _logger.warning(f"Cron: se encontraron {len(viajes)} viajes en curso para actualizar")
         viajes.fetch_device_positions()
-        
-    def llegada(self):
-        hora_llegada = datetime.now().time()
-        self.write({'tiempo_iniciado': 'true'})
-        _logger.warning(f"Tiempo iniciado: {self.tiempo_iniciado}")
-        _logger.warning(f"Hora llegada: {hora_llegada}")
     
-    def salida(self):
-        hora_salida = datetime.now().time()
-        self._calcular_tiempo()
-        _logger.warning(f"Hora salida: {hora_salida}")
 
-    def _calcular_tiempo(self):
-        _logger.warning("toy aqui")
-        for trip in self:
-            _logger.warning(f"viaje: {trip.hora_llegada}")
-            _logger.warning(f"Hora llegada: {trip.hora_llegada} - Hora salida: {trip.hora_salida}")
-            if trip.hora_llegada and trip.hora_salida:
-                trip.tiempo_usado = trip.hora_salida - trip.hora_llegada
-                _logger.warning(f"Tiempo usado: {trip.tiempo_usado}")
+
