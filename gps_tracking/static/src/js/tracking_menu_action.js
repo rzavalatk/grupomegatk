03922a281ff5825ceae7c6486babf4a5065c0680
Alexander Reyes <azelaya@megatk.com>
2025-05-13 11:29:29 -0600

    [AR]Prueba modulo localizacion

--- gps_tracking/static/src/js/tracking_menu_action.js
+++ gps_tracking/static/src/js/tracking_menu_action.js
@@ -1,3 +1,73 @@
+// odoo.define('gps_tracking.tracking_menu_action', function (require) {
+//     "use strict";
+
+//     const AbstractAction = require('web.AbstractAction');
+//     const core = require('web.core');
+
+//     const CustomCardMenu = AbstractAction.extend({
+//         template: 'TrackingCardMenu',
+
+//         events: {
+//             "click .btn-success": "_onClickIniciarViaje",
+//             "click .btn-warning": "_onClickPrueba",
+//         },
+
+//         willStart: async function () {
+//             const self = this;
+//             const result = await this._rpc({
+//                 model: 'gps.device.trip',
+//                 method: 'search_read',
+//                 domain: [["state", "=", "ongoing"]],
+//                 fields: ['check_in','device_id'],
+//                 limit: 1,
+//             }).then(function (result) {
+//                 self.current_trip = result.length ? result[0] : null;
+//             })
+//             return this._super.apply(this, arguments);
+//         },
+
+//         start: function () {  
+//             return this._super.apply(this, arguments);
+//         },
+
+//         renderElement: function () {
+//           this._super.apply(this, arguments);  
+//         },
+        
+//         _onClickIniciarViaje: function () {
+//             this._startTrip();
+//         },
+
+//         _startTrip: function() {
+//             var self = this;
+//             var l = self.$el.find("#id_device").val();
+//             if(l != '') {
+//                 if (/^\d{6}$/.test(l)) {
+//                     self.$el.find("#msg-text").text("");
+//                     self._rpc({
+//                         model: 'gps.device.trip',
+//                         method: '',
+//                         args:[l],
+//                     }).then(function (resultado) {
+
+//                     }).catch(function (error) {
+//                         console.error(error);
+//                     })
+//                 } else {
+//                     self.$el.find("#msg-text").text("El ID del dispositivo no es válido");
+//                     return;    
+//                 }
+//             } else if (l == '') {
+//                 self.$el.find("#msg-text").text("Ingrese el ID del dispositivo");
+//                 return;
+//             }
+//         },
+
+//     });
+
+//     core.action_registry.add('gps_tracking_tag', CustomCardMenu);  // Este es el tag que se llama en el XML
+//     return CustomCardMenu;
+// });
 odoo.define('gps_tracking.tracking_menu_action', function (require) {
     "use strict";
 
@@ -12,59 +82,65 @@ odoo.define('gps_tracking.tracking_menu_action', function (require) {
             "click .btn-warning": "_onClickPrueba",
         },
 
+        // Método asincrónico corregido
         willStart: async function () {
             const self = this;
+
             const result = await this._rpc({
                 model: 'gps.device.trip',
                 method: 'search_read',
                 domain: [["state", "=", "ongoing"]],
-                fields: ['check_in','device_id'],
+                fields: ['check_in', 'device_id'],
                 limit: 1,
-            }).then(function (result) {
-                self.current_trip = result.length ? result[0] : null;
-            })
-            return this._super.apply(this, arguments);
+            });
+
+            self.current_trip = result.length ? result[0] : null;
+
+            return AbstractAction.prototype.willStart.call(this);
         },
 
-        start: function () {  
-            return this._super.apply(this, arguments);
+        start: function () {
+            return AbstractAction.prototype.start.call(this);
         },
 
-        renderElement: function () {
-          this._super.apply(this, arguments);  
-        },
-        
         _onClickIniciarViaje: function () {
             this._startTrip();
         },
 
-        _startTrip: function() {
-            var self = this;
-            var l = self.$el.find("#id_device").val();
-            if(l != '') {
-                if (/^\d{6}$/.test(l)) {
-                    self.$el.find("#msg-text").text("");
-                    self._rpc({
-                        model: 'gps.device.trip',
-                        method: '',
-                        args:[l],
-                    }).then(function (resultado) {
+        _onClickPrueba: function () {
+            console.log("Botón de prueba clickeado");
+            console.log("Viaje actual:", this.current_trip);
+        },
 
-                    }).catch(function (error) {
-                        console.error(error);
-                    })
-                } else {
-                    self.$el.find("#msg-text").text("El ID del dispositivo no es válido");
-                    return;    
-                }
-            } else if (l == '') {
+        _startTrip: function () {
+            const self = this;
+            const deviceId = self.$el.find("#id_device").val();
+
+            if (!deviceId) {
                 self.$el.find("#msg-text").text("Ingrese el ID del dispositivo");
                 return;
             }
-        },
 
+            if (!/^\d{6}$/.test(deviceId)) {
+                self.$el.find("#msg-text").text("El ID del dispositivo no es válido");
+                return;
+            }
+
+            self.$el.find("#msg-text").text("");
+
+            self._rpc({
+                model: 'gps.device.trip',
+                method: 'iniciar_viaje_desde_ui', // <-- asegúrate de que este método exista
+                args: [deviceId],
+            }).then(function (resultado) {
+                console.log("Resultado del inicio de viaje:", resultado);
+            }).catch(function (error) {
+                console.error(error);
+                self.$el.find("#msg-text").text("Error al iniciar el viaje");
+            });
+        },
     });
 
-    core.action_registry.add('gps_tracking_tag', CustomCardMenu);  // Este es el tag que se llama en el XML
+    core.action_registry.add('gps_tracking_tag', CustomCardMenu);
     return CustomCardMenu;
 });
