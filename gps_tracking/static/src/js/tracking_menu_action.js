b1ad42bc4c690ac33d59711dbec343a2b24c791d
Alexander Reyes <azelaya@megatk.com>
2025-05-17 09:50:58 -0600

    [AR]Prueba modulo localizacion

--- gps_tracking/static/src/js/tracking_menu_action.js
+++ gps_tracking/static/src/js/tracking_menu_action.js
@@ -1,74 +1,3 @@
-// odoo.define('gps_tracking.tracking_menu_action', function (require) {
-//     "use strict";
-
-//     const AbstractAction = require('web.AbstractAction');
-//     const core = require('web.core');
-
-//     const CustomCardMenu = AbstractAction.extend({
-//         template: 'TrackingCardMenu',
-
-//         events: {
-//             "click .btn-success": "_onClickIniciarViaje",
-//             "click .btn-warning": "_onClickPrueba",
-//         },
-
-//         willStart: async function () {
-//             const self = this;
-//             const result = await this._rpc({
-//                 model: 'gps.device.trip',
-//                 method: 'search_read',
-//                 domain: [["state", "=", "ongoing"]],
-//                 fields: ['check_in','device_id'],
-//                 limit: 1,
-//             }).then(function (result) {
-//                 self.current_trip = result.length ? result[0] : null;
-//             })
-//             return this._super.apply(this, arguments);
-//         },
-
-//         start: function () {  
-//             return this._super.apply(this, arguments);
-//         },
-
-//         renderElement: function () {
-//           this._super.apply(this, arguments);  
-//         },
-        
-//         _onClickIniciarViaje: function () {
-//             this._startTrip();
-//         },
-
-//         _startTrip: function() {
-//             var self = this;
-//             var l = self.$el.find("#id_device").val();
-//             if(l != '') {
-//                 if (/^\d{6}$/.test(l)) {
-//                     self.$el.find("#msg-text").text("");
-//                     self._rpc({
-//                         model: 'gps.device.trip',
-//                         method: '',
-//                         args:[l],
-//                     }).then(function (resultado) {
-
-//                     }).catch(function (error) {
-//                         console.error(error);
-//                     })
-//                 } else {
-//                     self.$el.find("#msg-text").text("El ID del dispositivo no es válido");
-//                     return;    
-//                 }
-//             } else if (l == '') {
-//                 self.$el.find("#msg-text").text("Ingrese el ID del dispositivo");
-//                 return;
-//             }
-//         },
-
-//     });
-
-//     core.action_registry.add('gps_tracking_tag', CustomCardMenu);  // Este es el tag que se llama en el XML
-//     return CustomCardMenu;
-// });
-
 // //v2
 odoo.define('gps_tracking.tracking_menu_action', function (require) {
     "use strict";
@@ -92,6 +21,7 @@ odoo.define('gps_tracking.tracking_menu_action', function (require) {
 
         // Método asincrónico corregido
         willStart: async function () {
+            this._clearMessageTimeout();
             const self = this;
 
             const result = await this._rpc({
@@ -255,22 +185,30 @@ odoo.define('gps_tracking.tracking_menu_action', function (require) {
             this.renderElement();     // Re-renderiza el contenido desde el template
         },
 
+        _clearMessageTimeout: function () {
+            if (this._messageTimeout) {
+                console.log("🟢 Timeout cancelado", this._messageTimeout);
+                clearTimeout(this._messageTimeout);
+                this._messageTimeout = null;
+            }
+        },
+
         destroy: function () {
-        //Limpia temporizadores si existen
-    
-        console.log("🟢 Timeout cancelado", this._messageTimeout);
-        clearTimeout(this._messageTimeout);
-        this._messageTimeout = null;
-    
-        // 2. Limpiar todos los manejadores de eventos
-        this.$el.off(); // Elimina todos los listeners jQuery
+            //Limpia temporizadores si existen
         
-        // 3. Limpiar el DOM completamente
-        this.$el.empty();
-        this.$el.remove(); // Elimina el elemento del DOM
-    
-        // Llama al método padre para terminar correctamente
-        return this._super.apply(this, arguments);
+            console.log("🟢 Timeout cancelado", this._messageTimeout);
+            this._clearMessageTimeout();
+            this._messageTimeout = null;
+        
+            // 2. Limpiar todos los manejadores de eventos
+            this.$el.off(); // Elimina todos los listeners jQuery
+            
+            // 3. Limpiar el DOM completamente
+            this.$el.empty();
+            this.$el.remove(); // Elimina el elemento del DOM
+        
+            // Llama al método padre para terminar correctamente
+            return this._super.apply(this, arguments);
         },
     
     });
