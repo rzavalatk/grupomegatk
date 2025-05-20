980d612036fe2a75919341f47d3b6ac0881f775b
Alexander Reyes <azelaya@megatk.com>
2025-05-20 08:18:41 -0600

    [AR]Prueba modulo localizacion

--- gps_tracking/__manifest__.py
+++ gps_tracking/__manifest__.py
@@ -12,17 +12,17 @@
     'data': [
         'security/groups.xml',
         'security/ir.model.access.csv',
-        'views/gps_tracking_trip_view.xml',
         'views/gps_tracking_map_view.xml',
+        'views/gps_tracking_trip_view.xml',
         'views/gps_tracking_main_menu.xml',
         'data/cron.xml',
     ],
       'assets': {
         'web.assets_backend': [
-            '/gps_tracking/static/src/xml/tracking_menu_template.xml',
             '/gps_tracking/static/src/xml/tracking_map_template.xml',
-            '/gps_tracking/static/src/js/tracking_menu_action.js',
+            '/gps_tracking/static/src/xml/tracking_menu_template.xml',
             '/gps_tracking/static/src/js/tracking_map_action.js',
+            '/gps_tracking/static/src/js/tracking_menu_action.js',
             '/gps_tracking/static/lib/leaflet/leaflet.css',
             '/gps_tracking/static/lib/leaflet/leaflet.js',    
         ],
