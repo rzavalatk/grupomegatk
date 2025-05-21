2b74eade227f1cb38b4b7e0283e4a33b37893c46
Jiovanny <jmorales.megatk@gmail.com>
2025-05-21 14:29:30 -0600

    [JM] metodo de pago

---
+++ payment_mollie_official/models/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from . import mollie
+from . import payment_acquirer
+from . import payment_transaction
+from . import res_user
+from . import account_payment_method
+from . import account_move
+from . import account_payment_register
