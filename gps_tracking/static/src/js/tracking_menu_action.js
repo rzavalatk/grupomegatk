67cd2f5ef2d51d4de534f98153c07638c0d3638f
Alexander Reyes <azelaya@megatk.com>
2025-06-03 11:13:23 -0600

    [AR]Modificaciones modulo localizacion

--- gps_tracking/static/src/js/tracking_menu_action.js
+++ gps_tracking/static/src/js/tracking_menu_action.js
@@ -6,8 +6,8 @@ odoo.define('gps_tracking.tracking_menu_action', function (require) {
 
     const CustomCardMenu = AbstractAction.extend({
         template: 'TrackingCardMenu',
-        msg: null,
-        showMsg: null,
+        // msg: null,
+        // showMsg: null,
         events: {
             "click .btn-success": "_onClickIniciarViaje",
             "click .btn-warning": "_onClickFinalizarViaje",
@@ -75,17 +75,17 @@ odoo.define('gps_tracking.tracking_menu_action', function (require) {
                                 kwargs: { employee_id: await self._getCurrentEmployee() },
                             }).then(function (result) {
                                 console.log("Resultado del inicio de viaje:", result);
-                                self.showMsg = true;
-                                self.msg = {
-                                    title: "Viaje iniciado",
-                                    message: "El viaje ha sido iniciado con exito a la hora: " + result.start_time,
-                                };
-                                self.renderElement();
-                                setTimeout(function () {
-                                    self.showMsg = false;
-                                    self.msg = null;
-                                    self._reloadWidget();
-                                }, 5000) 
+                                self._reloadWidget();
+                                // self.showMsg = true;
+                                // self.msg = {
+                                //     title: "Viaje iniciado",
+                                //     message: "El viaje ha sido iniciado con exito a la hora: " + result.start_time,
+                                // };
+                                // self.renderElement();
+                                // setTimeout(function () {
+                                //     self.showMsg = false;
+                                //     self.msg = null;
+                                // }, 5000) 
                             }).catch(function (error) {
                                 console.error(error);
                                 $("#msg-text").text("Error al cargar el viaje");
@@ -109,55 +109,55 @@ odoo.define('gps_tracking.tracking_menu_action', function (require) {
                 args: [self.current_trip.device_id, await self._getCurrentEmployee()],
             }).then(function (resultado) {
                 console.log("Resultado del fin de viaje:", resultado);
-                var tiempo_final = self._diffTime(resultado.start_time, resultado.end_time);
-                self.showMsg = true;
-                self.msg = {
-                    title: "Viaje Finalizado",
-                    message: "Tiempo usado: " + tiempo_final.horas + ":" + tiempo_final.minutos + ":" + tiempo_final.segundos,
-                };
-                self.renderElement();
-                setTimeout(function () {
-                    self.showMsg = false;
-                    self.msg = null;
-                    self._reloadWidget();
-                }, 5000)
+                self._reloadWidget();
+                // var tiempo_final = self._diffTime(resultado.start_time, resultado.end_time);
+                // self.showMsg = true;
+                // self.msg = {
+                //     title: "Viaje Finalizado",
+                //     message: "Tiempo usado: " + tiempo_final.horas + ":" + tiempo_final.minutos + ":" + tiempo_final.segundos,
+                // };
+                // self.renderElement();
+                // setTimeout(function () {
+                //     self.showMsg = false;
+                //     self.msg = null;
+                // }, 5000)
             }).catch(function (error) {
                 console.error(error);
                 self.$el.find("#msg-text").text("Error al finalizar el viaje");
             });
         },
 
-        _diffTime: function (horaStr1, horaStr2) {
-            // Extraer horas, minutos y segundos
-            const [h1, m1, s1] = horaStr1.split(":").map(Number);
-            const [h2, m2, s2] = horaStr2.split(":").map(Number);
+        // _diffTime: function (horaStr1, horaStr2) {
+        //     // Extraer horas, minutos y segundos
+        //     const [h1, m1, s1] = horaStr1.split(":").map(Number);
+        //     const [h2, m2, s2] = horaStr2.split(":").map(Number);
     
-            // Usar una fecha ficticia común
-            const d1 = new Date(0, 0, 0, h1, m1, s1);
-            const d2 = new Date(0, 0, 0, h2, m2, s2);
+        //     // Usar una fecha ficticia común
+        //     const d1 = new Date(0, 0, 0, h1, m1, s1);
+        //     const d2 = new Date(0, 0, 0, h2, m2, s2);
     
-            // Calcular la diferencia en milisegundos
-            let diffMs = Math.abs(d1 - d2); // valor absoluto
+        //     // Calcular la diferencia en milisegundos
+        //     let diffMs = Math.abs(d1 - d2); // valor absoluto
 
-            // Convertir a horas, minutos y segundos
-            let horas = Math.floor(diffMs / (1000 * 60 * 60));
-            diffMs %= (1000 * 60 * 60);
-            let minutos = Math.floor(diffMs / (1000 * 60));
-            diffMs %= (1000 * 60);
-            let segundos = Math.floor(diffMs / 1000);
+        //     // Convertir a horas, minutos y segundos
+        //     let horas = Math.floor(diffMs / (1000 * 60 * 60));
+        //     diffMs %= (1000 * 60 * 60);
+        //     let minutos = Math.floor(diffMs / (1000 * 60));
+        //     diffMs %= (1000 * 60);
+        //     let segundos = Math.floor(diffMs / 1000);
 
-            if(horas < 10){
-                horas = "0" + horas;
-            }
-            if(minutos < 10){
-                minutos = "0" + minutos;
-            }
-            if(segundos < 10){
-                segundos = "0" + segundos;
-            }
+        //     if(horas < 10){
+        //         horas = "0" + horas;
+        //     }
+        //     if(minutos < 10){
+        //         minutos = "0" + minutos;
+        //     }
+        //     if(segundos < 10){
+        //         segundos = "0" + segundos;
+        //     }
         
-            return { horas, minutos, segundos };
-        },
+        //     return { horas, minutos, segundos };
+        // },
 
         _reloadWidget: async function () {
             const self = this;
