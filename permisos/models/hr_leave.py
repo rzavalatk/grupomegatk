6fff6dbb9558a79325bf7bdc014de72681347301
Alejandro Zelaya <jandro.zg123@gmail.com>
2025-06-17 15:12:41 -0600

    Prueba de permisos 3

--- permisos/models/hr_leave.py
+++ permisos/models/hr_leave.py
@@ -109,7 +109,7 @@ class HrLeave(models.Model):
     holiday_status_id = fields.Many2one(
         "hr.leave.type", compute='_compute_from_employee_id', store=True, string="Time Off Type", required=True, readonly=False,
         states={'cancel': [('readonly', True)], 'refuse': [('readonly', True)], 'validate1': [('readonly', True)], 'validate': [('readonly', True)]},
-        domain="[('company_id', '=?', employee_company_id), '|', ('has_valid_allocation', '=', True)]", tracking=True)
+        domain="[('company_id', '=?', employee_company_id)]", tracking=True)
     
     @api.onchange('request_hour_from_1')
     def _onchange_request_hour_from_1(self):
