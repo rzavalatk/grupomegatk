11611393231f58180e3f06a5a6020d6d3f1d4157
JiovannyM <jmorales.megatk@gmail.com>
2025-09-12 16:26:30 -0600

    agregando vista de arbol y many2one para sellecionar el tipo de condicion de garantia

--- product_warranty_management_odoo-16.0.1.0.0/product_warranty_management_odoo/models/__init__.py
+++ product_warranty_management_odoo-16.0.1.0.0/product_warranty_management_odoo/models/__init__.py
@@ -22,4 +22,4 @@
 from . import product_template
 from . import sale_order
 from . import warranty_claim
-#from . import warranty_conditions
+from . import warranty_conditions
--- product_warranty_management_odoo-16.0.1.0.0/product_warranty_management_odoo/models/warranty_conditions.py
+++ product_warranty_management_odoo-16.0.1.0.0/product_warranty_management_odoo/models/warranty_conditions.py
@@ -1,7 +1,7 @@
 from odoo import models, fields
 
 class WarrantyCondition(models.Model):
-    _name = 'warranty.condition'
+    _name = 'warranty.conditions'
     _description = 'Condiciones de garantía'
 
     name = fields.Char(string='Name', required=True)
