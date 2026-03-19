2922e195fbb6e839418f05a4141442ca2c8025ff
Alexander Reyes <areyes@megatk.com>
2026-03-19 16:01:30 -0600

    [AR] modificaciones no hacer obligatorio el uso de cuenta analitica

--- banks/models/account.py
+++ banks/models/account.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
-from odoo import models, fields, api, _
-from odoo.exceptions import UserError
+from odoo import models, fields, api
 
 
 class Account(models.Model):
@@ -13,6 +12,7 @@ class AccountMove(models.Model):
 
 	@api.model_create_multi
 	def create(self, vals_list):
+		# Keep analytic optional: only preload a default analytic account when configured on the account.
 		config = self.env['res.config.settings']
 		listcon = config.search([('company_id', '=', self.env.user.company_id.id)])
 		acti = ""
@@ -25,15 +25,8 @@ class AccountMove(models.Model):
 				account_id = vals.get('account_id')
 				if account_id:
 					account = self.env['account.account'].browse(account_id)
-					# En Odoo 16 ya no existe user_type_id; se usa account_type
-					if account.account_type in ('asset_receivable', 'liability_payable'):
-						if not vals.get('analytic_account_id'):
-							if account.analytic_id:
-								vals['analytic_account_id'] = account.analytic_id.id
-							else:
-								raise UserError(_(
-									'Cuenta analítica requerida en la línea con cuenta: %s %s descripción: %s'
-								) % (account.code, account.name, vals.get('name', '')))
+					if not vals.get('analytic_account_id') and account.analytic_id:
+						vals['analytic_account_id'] = account.analytic_id.id
 		return super(AccountMove, self).create(vals_list)
 	
 
