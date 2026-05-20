0369f8afacf237a90a68c14740f359d93837b441
Alexander Reyes <areyes@megatk.com>
2026-05-20 16:10:21 -0600

    [AR]modulo de tickets

--- helpdesk_custom_megatk/models/helpdesk_ticket.py
+++ helpdesk_custom_megatk/models/helpdesk_ticket.py
@@ -12,3 +12,15 @@ class HelpdeskTicket(models.Model):
     )
     x_tecnico_diagnostico = fields.Text(string="Diagnostico tecnico")
     x_tecnico_solucion = fields.Text(string="Solucion aplicada")
+
+    def action_print_orden_ingreso(self):
+        """Imprime la orden de ingreso del ticket"""
+        return self.env.ref("helpdesk_custom_megatk.helpdesk_ticket_orden_ingreso").report_action(self)
+
+    def action_print_visita_tecnica(self):
+        """Imprime la visita técnica del ticket"""
+        return self.env.ref("helpdesk_custom_megatk.helpdesk_ticket_visita_tecnica").report_action(self)
+
+    def action_print_entrega_equipo(self):
+        """Imprime la entrega de equipo del ticket"""
+        return self.env.ref("helpdesk_custom_megatk.helpdesk_ticket_entrega_equipo").report_action(self)
