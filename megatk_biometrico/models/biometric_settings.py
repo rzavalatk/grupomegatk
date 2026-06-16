f29b247a0835ec2713eaa6057bae7a9cd854898a
JiovannyM <jmorales.megatk@gmail.com>
2026-06-16 16:25:26 -0600

    [JM] prueba de conexion

--- megatk_biometrico/models/biometric_settings.py
+++ megatk_biometrico/models/biometric_settings.py
@@ -7,5 +7,10 @@ class BiometricSettings(models.TransientModel):
     server_url = fields.Char(string='URL del Servidor', config_parameter='biometric.server_url')
     api_token = fields.Char(string='Token API', config_parameter='biometric.api_token')
     websocket_url = fields.Char(string='WebSocket URL', config_parameter='biometric.websocket_url')
-    default_device_group = fields.Many2one('res.groups', string='Grupo de Dispositivos', config_parameter='biometric.default_device_group')
+    default_device_group = fields.Many2one(
+        'res.groups',
+        string='Grupo de Dispositivos',
+        config_parameter='biometric.default_device_group',
+        default_model='res.groups'
+    )
     allow_command_execution = fields.Boolean(string='Permitir ejecución de comandos', default=True, config_parameter='biometric.allow_command_execution')
