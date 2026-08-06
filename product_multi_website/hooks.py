3a35e2fdcc0373d50c00ea13e3fe8eea2c207de2
JiovannyM <jmorales.megatk@gmail.com>
2026-08-06 11:25:32 -0600

    [JM] prueba de website multiple

--- product_multi_website/hooks.py
+++ product_multi_website/hooks.py
@@ -1,12 +1,8 @@
-from odoo import api, SUPERUSER_ID
-
-
-def post_init_hook(cr, registry):
+def post_init_hook(env):
     """Copia el website_id historico al nuevo campo website_ids_multi al instalar.
 
     Esto evita que los productos ya configurados pierdan su restriccion de sitio.
     """
-    env = api.Environment(cr, SUPERUSER_ID, {})
     products = env["product.template"].sudo().search([("website_id", "!=", False)])
     for product in products:
         if product.website_id and product.website_id not in product.website_ids_multi:
