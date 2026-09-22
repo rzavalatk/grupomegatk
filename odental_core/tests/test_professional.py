7d547feb2bd9cf531e685e1f9a61a202149773a5
ajz04megatk <89929784+ajz04megatk@users.noreply.github.com>
2026-09-21 21:04:36 -0600

    Update autopost_bills default value in tests

--- odental_core/tests/test_professional.py
+++ odental_core/tests/test_professional.py
@@ -1,12 +1,13 @@
 from odoo.exceptions import ValidationError
-from odoo.tests.common import TransactionCase
+from odoo.tests.common import TransactionCase, tagged
 
 
+@tagged("post_install", "-at_install")
 class TestODentalProfessional(TransactionCase):
     def _partner_compatible_values(self, **values):
         """Supply defaults required by optional Grupo Mega partner extensions."""
         if "autopost_bills" in self.env["res.partner"]._fields:
-            values.setdefault("autopost_bills", False)
+            values.setdefault("autopost_bills", "never")
         return values
 
     @classmethod
