ec4428fd47a253b355bf6b0c608e9883552fd5f2
ajz04megatk <89929784+ajz04megatk@users.noreply.github.com>
2026-09-26 15:36:16 -0600

    Preparar contactos de compañías de prueba compatibles con Grupo Mega

--- odental_clinical/tests/test_clinical.py
+++ odental_clinical/tests/test_clinical.py
@@ -117,9 +117,20 @@ class TestODentalClinical(TransactionCase):
 @tagged("post_install", "-at_install")
 class TestODentalPatientFlow(TransactionCase):
     @classmethod
+    def _test_company(cls, name):
+        values = {'name': name}
+        if 'autopost_bills' in cls.env['res.partner']._fields:
+            values['autopost_bills'] = 'never'
+        partner = cls.env['res.partner'].create(values)
+        # Grupo Mega assigns every new contact to the current company.
+        # A company address must be shared before creating its warehouse.
+        partner.write({'company_id': False})
+        return cls.env['res.company'].create({'name': name, 'partner_id': partner.id})
+
+    @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        cls.company = cls.env['res.company'].with_context(default_autopost_bills='never').create({'name': 'Dental flow test'})
+        cls.company = cls._test_company('Dental flow test')
         values = {
             'name': 'Dental flow professional',
             'login': 'odental-flow-professional',
@@ -154,7 +165,7 @@ class TestODentalPatientFlow(TransactionCase):
         self.assertEqual(self.patients.with_context(default_organization_id=self.organization.id).default_get(['organization_id'])['organization_id'], self.organization.id)
 
     def test_no_default_from_another_company(self):
-        other_company = self.env['res.company'].with_context(default_autopost_bills='never').create({'name': 'Empty dental company'})
+        other_company = self._test_company('Empty dental company')
         self.operator.write({'company_ids': [(4, other_company.id)]})
         self.assertFalse(self.patients.with_company(other_company).default_get(['organization_id']).get('organization_id'))
 
