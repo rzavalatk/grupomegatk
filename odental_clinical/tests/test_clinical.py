760fc86e951b9be5ab08d73c71ac50ff9bec0e27
ajz04megatk <89929784+ajz04megatk@users.noreply.github.com>
2026-09-26 15:31:48 -0600

    Probar organizaciones, duplicados y permisos en flujo del paciente

--- odental_clinical/tests/test_clinical.py
+++ odental_clinical/tests/test_clinical.py
@@ -112,3 +112,73 @@ class TestODentalClinical(TransactionCase):
         )
         with self.assertRaises(ValidationError):
             consent.action_sign()
+
+
+class TestODentalPatientFlow(TransactionCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.company = cls.env['res.company'].create({'name': 'Dental flow test'})
+        values = {
+            'name': 'Dental flow professional',
+            'login': 'odental-flow-professional',
+            'company_id': cls.company.id,
+            'company_ids': [(6, 0, cls.company.ids)],
+            'groups_id': [(6, 0, [cls.env.ref('base.group_user').id,
+                                  cls.env.ref('odental_core.group_odental_professional').id])],
+        }
+        if 'autopost_bills' in cls.env['res.users']._fields:
+            values['autopost_bills'] = 'never'
+        cls.operator = cls.env['res.users'].with_context(no_reset_password=True).create(values)
+        cls.organization = cls.env['odental.organization'].create({
+            'name': 'Authorized dental organization', 'code': 'FLOW-A',
+            'company_id': cls.company.id, 'user_ids': [(4, cls.operator.id)],
+        })
+        cls.hidden = cls.env['odental.organization'].create({
+            'name': 'Other dental organization', 'code': 'FLOW-B',
+            'company_id': cls.company.id,
+        })
+        cls.patients = cls.env['odental.patient'].with_user(cls.operator).with_company(cls.company)
+        cls.patient = cls.patients.create({'name': 'Flow patient', 'organization_id': cls.organization.id})
+
+    def test_default_uses_only_authorized_organization(self):
+        self.assertEqual(self.patients.default_get(['organization_id'])['organization_id'], self.organization.id)
+
+    def test_ambiguous_organization_is_not_chosen(self):
+        self.hidden.write({'user_ids': [(4, self.operator.id)]})
+        self.assertFalse(self.patients.default_get(['organization_id']).get('organization_id'))
+
+    def test_context_choice_is_preserved(self):
+        self.assertFalse(self.patients.with_context(default_organization_id=False).default_get(['organization_id'])['organization_id'])
+        self.assertEqual(self.patients.with_context(default_organization_id=self.organization.id).default_get(['organization_id'])['organization_id'], self.organization.id)
+
+    def test_no_default_from_another_company(self):
+        other_company = self.env['res.company'].create({'name': 'Empty dental company'})
+        self.operator.write({'company_ids': [(4, other_company.id)]})
+        self.assertFalse(self.patients.with_company(other_company).default_get(['organization_id']).get('organization_id'))
+
+    def test_open_new_record_does_not_save_it(self):
+        action = self.patient.action_open_clinical_record()
+        self.assertFalse(action['res_id'])
+        self.assertEqual(action['context']['default_patient_id'], self.patient.id)
+        self.assertFalse(self.env['odental.clinical.record'].search_count([('patient_id', '=', self.patient.id)]))
+
+    def test_open_reuses_closed_record(self):
+        record = self.env['odental.clinical.record'].create({'patient_id': self.patient.id})
+        record.action_close()
+        action = self.patient.action_open_clinical_record()
+        self.assertEqual(action['res_id'], record.id)
+        self.assertEqual(record.state, 'closed')
+        self.assertEqual(self.env['odental.clinical.record'].search_count([('patient_id', '=', self.patient.id)]), 1)
+
+    def test_other_organization_patient_is_denied(self):
+        from odoo.exceptions import AccessError
+        hidden_patient = self.env['odental.patient'].create({'name': 'Other flow patient', 'organization_id': self.hidden.id})
+        with self.assertRaises(AccessError):
+            hidden_patient.with_user(self.operator).action_open_clinical_record()
+
+    def test_reception_role_cannot_open_clinical_record(self):
+        from odoo.exceptions import AccessError
+        self.operator.write({'groups_id': [(6, 0, [self.env.ref('base.group_user').id, self.env.ref('odental_core.group_odental_user').id])]})
+        with self.assertRaises(AccessError):
+            self.patient.action_open_clinical_record()
